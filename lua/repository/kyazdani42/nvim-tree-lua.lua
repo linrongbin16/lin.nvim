d1e77082ac8815afc24da9a82787229ec316e0fa
Rongbin Lin <rlin@flexport.com>
2023-01-16 09:35:11 +0800

    remove coc, move nvim-tree setup to lua

---
+++ lua/repository/kyazdani42/nvim-tree-lua.lua
@@ -0,0 +1,49 @@
+local lin_keymap = {
+    -- navigation
+    { key = "l", action = "edit" }, -- open folder or edit file
+    { key = "h", action = "close_node" }, -- close folder
+
+    -- copy/paste/cut
+    { key = "X", action = "cut" },
+    { key = "C", action = "copy" },
+    { key = "V", action = "paste" },
+}
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+require 'nvim-tree'.setup {
+    open_on_setup = true,
+    open_on_setup_file = true,
+    view = {
+        width = 40,
+        side = "left",
+        signcolumn = "yes",
+        mappings = {
+            custom_only = false,
+            list = lin_keymap,
+        },
+    },
+    renderer = {
+        highlight_git = true,
+        indent_markers = {
+            enable = true,
+        },
+        icons = {
+            webdev_colors = true,
+            git_placement = "signcolumn",
+            glyphs = {
+                default = "",
+                symlink = "",
+            },
+        },
+    },
+    update_focused_file = {
+        enable      = true,
+        update_root = true,
+    },
+    git = {
+        enable = true,
+        ignore = false,
+    },
+}
