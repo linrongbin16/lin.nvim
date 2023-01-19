739e04b6ed6aaf60a8dd3dd501bbed4583cf231f
linrongbin16 <linrongbin16@outlook.com>
2023-01-19 16:51:19 +0800

    format

--- generator.py
+++ generator.py
@@ -554,9 +554,13 @@ PLUGINS = [
         tag=Tag.COLORSCHEME,
     ),
     Plugin("shaunsingh", "nord.nvim", color="nord", tag=Tag.COLORSCHEME),
-    Plugin("sonph", "onehalf", 
-           post="rtp = 'vim/'",
-           color="onehalfdark", tag=Tag.COLORSCHEME),
+    Plugin(
+        "sonph",
+        "onehalf",
+        post="rtp = 'vim/'",
+        color="onehalfdark",
+        tag=Tag.COLORSCHEME,
+    ),
     Plugin("srcery-colors", "srcery-vim", color="srcery", tag=Tag.COLORSCHEME),
     # Highlight
     Plugin(
