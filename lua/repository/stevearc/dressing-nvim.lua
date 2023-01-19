3aaf63cbd9cf3323a00436fbe9e59d272c76d32c
linrongbin16 <linrongbin16@outlook.com>
2023-01-19 14:04:17 +0800

    optimize dressing working with nvim-tree

--- lua/repository/stevearc/dressing-nvim.lua
+++ lua/repository/stevearc/dressing-nvim.lua
@@ -1,6 +1,22 @@
+local window_transparency = 90
+
+local function LinGetConfig()
+    -- put UI box in global editor if window is too small
+    if vim.api.nvim_win_get_width(0) < 60 then
+        return {
+            relative = "editor",
+            prefer_width = 80,
+        }
+    end
+end
+
 require('dressing').setup({
     input = {
         border = vim.g.lin_constants.ui.border,
+        win_options = {
+            winblend = window_transparency,
+        },
+        get_config = LinGetConfig,
     },
     select = {
         nui = {
@@ -10,6 +26,10 @@ require('dressing').setup({
         },
         builtin = {
             border = vim.g.lin_constants.ui.border,
-        }
-    }
+            win_options = {
+                winblend = window_transparency,
+            },
+        },
+        get_config = LinGetConfig,
+    },
 })
