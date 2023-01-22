b3afffce47ac31578f2c938527c9f02bb50a4dd0
linrongbin16 <linrongbin16@outlook.com>
2023-01-23 01:17:15 +0800

    optimize config

--- lua/repository/lewis6991/gitsigns-nvim.lua
+++ lua/repository/lewis6991/gitsigns-nvim.lua
@@ -1,11 +1,11 @@
 require('gitsigns').setup {
     signs = {
-        add          = { hl = 'GitSignsAdd', text = '+', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
-        change       = { hl = 'GitSignsChange', text = '~', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        delete       = { hl = 'GitSignsDelete', text = '_', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        topdelete    = { hl = 'GitSignsDelete', text = '‾', numhl = 'GitSignsDeleteNr', linehl = 'GitSignsDeleteLn' },
-        changedelete = { hl = 'GitSignsChange', text = '~_', numhl = 'GitSignsChangeNr', linehl = 'GitSignsChangeLn' },
-        untracked    = { hl = 'GitSignsAdd', text = '┆', numhl = 'GitSignsAddNr', linehl = 'GitSignsAddLn' },
+        add          = { text = '+' },
+        change       = { text = '~' },
+        delete       = { text = '_' },
+        topdelete    = { text = '‾' },
+        changedelete = { text = '~_' },
+        untracked    = { text = '┆' },
     },
     current_line_blame_opts = {
         delay = 300,
