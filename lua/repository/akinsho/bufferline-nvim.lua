dca85b6dbb103e8d00f0eff5dfeb7c91a5905a49
linrongbin16 <linrongbin16@outlook.com>
2023-01-23 00:29:08 +0800

    remove bufferline

--- lua/repository/akinsho/bufferline-nvim.lua
+++
@@ -1,7 +0,0 @@
-require('bufferline').setup {
-    options = {
-        numbers = "both",
-        max_name_length = 40,
-        max_prefix_length = 35, -- prefix used when a buffer is de-duplicated
-    }
-}
