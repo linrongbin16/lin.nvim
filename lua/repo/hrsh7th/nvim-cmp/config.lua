9d78c0e34d2aae17fe2ae1656e3eea8a72ace65c
linrongbin16 <linrongbin16@outlook.com>
2023-01-29 06:17:13 +0800

    remove cmp_git

--- lua/repo/hrsh7th/nvim-cmp/config.lua
+++ lua/repo/hrsh7th/nvim-cmp/config.lua
@@ -95,9 +95,8 @@ cmp.setup({
 
 -- Set configuration for specific filetype.
 cmp.setup.filetype('gitcommit', {
-    sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-    }, {
+    sources = cmp.config.sources(
+    {
         { name = 'buffer' },
     })
 })
