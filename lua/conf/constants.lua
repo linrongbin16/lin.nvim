dc2fe71078e1c26d9ab8df6e840b4b418fb4a403
Rongbin Lin <rlin@flexport.com>
2023-01-28 14:50:22 +0800

    better icon

--- lua/conf/constants.lua
+++ lua/conf/constants.lua
@@ -3,19 +3,19 @@
 local M = {}
 
 M.lsp = {
-    diagnostics = {
-        signs = {
-            ['error'] = '', -- nf-fa-times \uf00d
-            ['warning'] = '', -- nf-fa-warning \uf071
-            ['info'] = '', -- nf-fa-info (nf-fa-info_circle) \uf129
-            ['hint'] = '', -- nf-mdi-lightbulb \uf834
-            ['ok'] = '', -- nf-fa-check \uf00c
-        }
-    },
+	diagnostics = {
+		signs = {
+			["error"] = "", -- nf-fa-times \uf00d
+			["warning"] = "", -- nf-fa-warning \uf071
+			["info"] = "", -- nf-fa-info_circle \uf05a
+			["hint"] = "", -- nf-mdi-lightbulb \uf834
+			["ok"] = "", -- nf-fa-check \uf00c
+		},
+	},
 }
 
 M.ui = {
-    border = 'rounded', -- border options: single,double,rounded,solid,shadow
+	border = "rounded", -- border options: single,double,rounded,solid,shadow
 }
 
 return M
