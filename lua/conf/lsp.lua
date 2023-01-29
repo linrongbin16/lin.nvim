23d62443bce1c1fb86e99f149cf25005726e42b5
Rongbin Lin <rlin@flexport.com>
2023-01-29 13:29:43 +0800

    optimize LSP ui

--- lua/conf/lsp.lua
+++ lua/conf/lsp.lua
@@ -1,32 +1,32 @@
-local constants = require('conf/constants')
+local constants = require("conf/constants")
 
 -- key mappings
-vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP actions",
     callback = function()
         local bufmap = function(mode, lhs, rhs)
-            local opts = { buffer = true, noremap = true, silent = true, }
+            local opts = { buffer = true, noremap = true, silent = true }
             vim.keymap.set(mode, lhs, rhs, opts)
         end
-        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-        bufmap('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-        bufmap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-        bufmap('n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>')
-        bufmap('n', '<Leader>cf', '<cmd>lua vim.lsp.buf.format()<cr>')
-        bufmap('x', '<Leader>cf', '<cmd>lua vim.lsp.buf.format()<cr>')
-        bufmap('n', '<Leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>')
-        bufmap('x', '<Leader>ca', '<cmd>lua vim.lsp.buf.range_code_action()<cr>')
-        bufmap('n', '<Leader>df', '<cmd>lua vim.diagnostic.open_float()<cr>')
-        bufmap('n', '<Leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>')
-        bufmap('n', '<Leader>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>')
-        bufmap('n', '<Leader>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>')
-        bufmap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<cr>')
-        bufmap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<cr>')
-    end
+        bufmap("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>")
+        bufmap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>")
+        bufmap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>")
+        bufmap("n", "gt", "<cmd>lua vim.lsp.buf.type_definition()<cr>")
+        bufmap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>")
+        bufmap("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>")
+        bufmap("n", "<C-k>", "<cmd>lua vim.lsp.buf.signature_help()<cr>")
+        bufmap("n", "<Leader>rn", "<cmd>lua vim.lsp.buf.rename()<cr>")
+        bufmap("n", "<Leader>cf", "<cmd>lua vim.lsp.buf.format()<cr>")
+        bufmap("x", "<Leader>cf", "<cmd>lua vim.lsp.buf.format()<cr>")
+        bufmap("n", "<Leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+        bufmap("x", "<Leader>ca", "<cmd>lua vim.lsp.buf.range_code_action()<cr>")
+        bufmap("n", "<Leader>df", "<cmd>lua vim.diagnostic.open_float()<cr>")
+        bufmap("n", "<Leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<cr>")
+        bufmap("n", "<Leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<cr>")
+        bufmap("n", "<Leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<cr>")
+        bufmap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<cr>")
+        bufmap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<cr>")
+    end,
 })
 
 -- diagnostic
@@ -34,32 +34,27 @@ local diagnostics_sign = function(opts)
     vim.fn.sign_define(opts.name, {
         texthl = opts.name,
         text = opts.text,
-        numhl = ''
+        numhl = "",
     })
 end
 
-diagnostics_sign({ name = 'DiagnosticSignError', text = constants.lsp.diagnostics.signs['error'] })
-diagnostics_sign({ name = 'DiagnosticSignWarn', text = constants.lsp.diagnostics.signs['warning'] })
-diagnostics_sign({ name = 'DiagnosticSignInfo', text = constants.lsp.diagnostics.signs['info'] })
-diagnostics_sign({ name = 'DiagnosticSignHint', text = constants.lsp.diagnostics.signs['hint'] })
+diagnostics_sign({ name = "DiagnosticSignError", text = constants.lsp.diagnostics.signs["error"] })
+diagnostics_sign({ name = "DiagnosticSignWarn", text = constants.lsp.diagnostics.signs["warning"] })
+diagnostics_sign({ name = "DiagnosticSignInfo", text = constants.lsp.diagnostics.signs["info"] })
+diagnostics_sign({ name = "DiagnosticSignHint", text = constants.lsp.diagnostics.signs["hint"] })
 
 vim.diagnostic.config({
     virtual_text = false,
     severity_sort = true,
     float = {
         border = constants.ui.border,
-        source = 'always',
-        header = '',
-        prefix = '',
+        source = "always",
+        header = "",
+        prefix = "",
     },
 })
 
 -- hover/signatureHelp
-vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
-    vim.lsp.handlers.hover,
-    { border = constants.ui.border }
-)
-vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
-    vim.lsp.handlers.signature_help,
-    { border = constants.ui.border }
-)
+vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = constants.ui.border })
+vim.lsp.handlers["textDocument/signatureHelp"] =
+vim.lsp.with(vim.lsp.handlers.signature_help, { border = constants.ui.border })
--- lua/repo/stevearc/dressing-nvim/config.lua
+++ lua/repo/stevearc/dressing-nvim/config.lua
@@ -1,38 +1,42 @@
 local window_transparency = 0
 
 local function GetConfig()
-    -- put UI box in global editor if window is too small
-    if vim.api.nvim_win_get_width(0) < 60 then
-        return {
-            relative = "editor",
-            prefer_width = 80,
-        }
-    end
+	-- put UI box in global editor if window is too small
+	if vim.api.nvim_win_get_width(0) < 60 then
+		return {
+			relative = "editor",
+			prefer_width = 80,
+		}
+	end
 end
 
-local constants = require('conf/constants')
+local constants = require("conf/constants")
 
-require('dressing').setup({
-    input = {
-        border = constants.ui.border,
-        win_options = {
-            winblend = window_transparency,
-        },
-        get_config = GetConfig,
-    },
-    select = {
-        nui = {
-            border = {
-                style = constants.ui.border,
-            },
-        },
-        builtin = {
-            border = constants.ui.border,
-            win_options = {
-                winblend = window_transparency,
-            },
-        },
-        get_config = GetConfig,
-    },
+require("dressing").setup({
+	input = {
+		border = constants.ui.border,
+		win_options = {
+			winblend = window_transparency,
+		},
+		get_config = GetConfig,
+		override = function(conf)
+			conf.col = -1
+			conf.row = 0
+			return conf
+		end,
+	},
+	select = {
+		nui = {
+			border = {
+				style = constants.ui.border,
+			},
+		},
+		builtin = {
+			border = constants.ui.border,
+			win_options = {
+				winblend = window_transparency,
+			},
+		},
+		get_config = GetConfig,
+	},
 })
-
