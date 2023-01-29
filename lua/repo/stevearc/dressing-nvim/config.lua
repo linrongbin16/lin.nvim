73c18606cac43a11510fc7261634067b617e492e
Rongbin Lin <rlin@flexport.com>
2023-01-29 16:13:51 +0800

    optimize input UI width

--- lua/repo/stevearc/dressing-nvim/config.lua
+++ lua/repo/stevearc/dressing-nvim/config.lua
@@ -1,21 +1,19 @@
-local window_transparency = 0
-
-local function GetConfig()
-	-- put UI box in global editor if window is too small
-	if vim.api.nvim_win_get_width(0) <= 40 then
-		return {
-			relative = "editor",
-			prefer_width = 60,
-		}
-	end
-end
-
 local constants = require("conf/constants")
 local close_mappings = { ["<ESC>"] = "Close", ["<C-[>"] = "Close", ["<C-c>"] = "Close" }
+local window_transparency = 0
+local window_width = 55
+
+local function OverrideConfig(conf)
+	if vim.api.nvim_win_get_width(0) <= window_width then
+		conf.width = window_width
+	end
+	return conf
+end
 
 require("dressing").setup({
 	input = {
 		border = constants.ui.border,
+		min_width = { window_width, 0.2 },
 		win_options = {
 			winblend = window_transparency,
 		},
@@ -23,7 +21,7 @@ require("dressing").setup({
 			n = close_mappings,
 			i = close_mappings,
 		},
-		get_config = GetConfig,
+		-- override = OverrideConfig,
 	},
 	select = {
 		nui = {
@@ -38,6 +36,5 @@ require("dressing").setup({
 			},
 			mappings = close_mappings,
 		},
-		get_config = GetConfig,
 	},
 })
