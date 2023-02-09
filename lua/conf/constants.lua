7339e1de2c0705d10fe48c4c1df875445201aecb
Rongbin Lin <rlin@flexport.com>
2023-02-09 11:30:22 +0800

    choose another icon for hint, instead of lightbulb

--- lua/conf/constants.lua
+++ lua/conf/constants.lua
@@ -8,7 +8,8 @@ M.lsp = {
             ["error"] = "", -- nf-fa-times \uf00d
             ["warning"] = "", -- nf-fa-warning \uf071
             ["info"] = "", -- nf-fa-info_circle \uf05a
-            ["hint"] = "", -- nf-mdi-lightbulb \uf834
+            -- ["hint"] = "", -- nf-mdi-lightbulb \uf834, used for codeAction in vscode, choose another icon
+            ["hint"] = "", -- : nf-fa-flag \uf024,  nf-fa-bell \uf0f3
             ["ok"] = "", -- nf-fa-check \uf00c
         },
     },
@@ -18,7 +19,8 @@ M.ui = {
     border = "rounded", -- border options: single,double,rounded,solid,shadow
 }
 
-M.perf = { -- performance
+M.perf = {
+    -- performance
     filesystem = {
         maxsize = 1024 * 1024 * 10, -- 10MB
     },
