4584d722b8a52ce2ef65cf40eebd6af4a8f9cede
Rongbin Lin <rlin@flexport.com>
2023-02-14 13:13:26 +0800

    remove opinioned lsp servers

--- temp/lspservers.lua
+++ temp/lspservers.lua
@@ -15,7 +15,7 @@ local null_ls = require("null-ls")
 local constants = require("conf/constants")
 
 local function attach_ext(client, bufnr)
-  -- attach navic to working with multiple buffers/tabs
+  -- attach navic to work with multiple tabs
   if client.server_capabilities["documentSymbolProvider"] then
     require("nvim-navic").attach(client, bufnr)
   end
@@ -25,29 +25,10 @@ end
 local embeded_servers = {
   -- clang
   "clangd",
-  -- cmake
-  "cmake",
-  -- css
-  "cssls",
-  "cssmodules_ls",
-  -- html
-  "html",
-  -- json
-  "jsonls",
-  -- js/ts
-  "tsserver",
   -- lua
   "lua_ls",
-  -- python
-  "pyright",
   -- vim
   "vimls",
-  -- xml
-  "lemminx",
-  -- yaml
-  "yamlls",
-  -- bash/powershell
-  vim.fn.has("win32") > 0 and "powershell_es" or "bashls",
 }
 local embeded_servers_setups = {
   -- default setup
@@ -105,18 +86,11 @@ local embeded_servers_setups = {
 
 -- { null-ls's config
 local embeded_nullls = {
-  -- js/ts
-  "eslint_d",
-  "prettierd",
   -- lua
-  "stylua", -- Better lua formatter
-  -- python
-  "black", -- Since pyright doesn't include code format.
-  "isort", -- Use black/isort as code formatter.
+  "stylua", -- better lua formatter
+  -- vim
+  "vint", -- vim lint
 }
-if vim.fn.has("win32") <= 0 then
-  table.insert(embeded_nullls, "shfmt")
-end
 local embeded_nullls_setups = {
   -- default setup
   function(source, methods)
