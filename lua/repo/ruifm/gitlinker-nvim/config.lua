1d41262607c6be25c2d3bbc234f6bfe248c21b62
linrongbin16 <linrongbin16@outlook.com>
2023-02-17 00:26:12 +0800

    fix gitlinker keys

--- lua/repo/ruifm/gitlinker-nvim/config.lua
+++ lua/repo/ruifm/gitlinker-nvim/config.lua
@@ -1,3 +1,3 @@
 require("gitlinker").setup({
-    mappings = nil,
+    mappings = "<leader>gl",
 })
--- lua/repo/ruifm/gitlinker-nvim/keys.lua
+++ lua/repo/ruifm/gitlinker-nvim/keys.lua
@@ -1,18 +1,19 @@
 local map_lazy = require("conf/keymap").map_lazy
 
 local M = {
-    map_lazy(
-        "n",
-        "<leader>gl",
-        '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
-        { silent = true, desc = "Copy git link to clipboard" }
-    ),
-    map_lazy(
-        "x",
-        "<leader>gl",
-        '<cmd>lua require"gitlinker".get_buf_range_url("x", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
-        { silent = true, desc = "Copy git link to clipboard" }
-    ),
+    "<leader>gl",
+    -- map_lazy(
+    --     "n",
+    --     "<leader>gl",
+    --     '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
+    --     { silent = true, desc = "Copy git link to clipboard" }
+    -- ),
+    -- map_lazy(
+    --     "x",
+    --     "<leader>gl",
+    --     '<cmd>lua require"gitlinker".get_buf_range_url("x", {action_callback = require"gitlinker.actions".copy_to_clipboard})<cr>',
+    --     { silent = true, desc = "Copy git link to clipboard" }
+    -- ),
     map_lazy(
         "n",
         "<leader>gL",
