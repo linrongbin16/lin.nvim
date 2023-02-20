bf4cd620a03d8132c30732e46d83ea3a1e44a948
Rongbin Lin <rlin@flexport.com>
2023-02-20 10:09:00 +0800

    update gitlinker mappings

--- lua/repo/linrongbin16/gitlinker-nvim/keys.lua
+++ lua/repo/linrongbin16/gitlinker-nvim/keys.lua
@@ -4,13 +4,13 @@ local M = {
     map_lazy(
         "n",
         "<leader>gl",
-        '<cmd>lua require"gitlinker".get_buf_range_url("n", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        '<cmd>lua require"gitlinker".get_buf_range_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
         { silent = true, desc = "Open git link in browser" }
     ),
     map_lazy(
         "v",
         "<leader>gl",
-        '<cmd>lua require"gitlinker".get_buf_range_url("v", {action_callback = require"gitlinker.actions".open_in_browser})<cr>',
+        '<cmd>lua require"gitlinker".get_buf_range_url({action_callback = require"gitlinker.actions".open_in_browser})<cr>',
         { desc = "Open git link in browser" }
     ),
 }
