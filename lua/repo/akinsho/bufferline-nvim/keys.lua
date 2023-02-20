b83cc753bab38bc5879999c27d99e543d1fe1384
Rongbin Lin <rlin@flexport.com>
2023-02-20 10:03:46 +0800

    rewrite bufferline keys

--- lua/repo/akinsho/bufferline-nvim/keys.lua
+++ lua/repo/akinsho/bufferline-nvim/keys.lua
@@ -1,6 +1,42 @@
 local keymap = require("conf/keymap")
 
-local M = {}
+local M = {
+    -- go to the last buffer
+    keymap.map_lazy(
+        "n",
+        "<leader>0",
+        keymap.exec("lua require('bufferline').go_to_buffer(-1, true)"),
+        { desc = "Go to the last buffer" }
+    ),
+
+    -- go to next/previous buffer
+    keymap.map_lazy(
+        "n",
+        "]b",
+        keymap.exec("BufferLineCycleNext"),
+        { desc = "Go to next buffer" }
+    ),
+    keymap.map_lazy(
+        "n",
+        "[b",
+        keymap.exec("BufferLineCyclePrev"),
+        { desc = "Go to previous buffer" }
+    ),
+
+    -- move/re-order buffer to next/previous position
+    keymap.map_lazy(
+        "n",
+        "<leader>.",
+        keymap.exec("BufferLineMoveNext"),
+        { desc = "Move buffer to next" }
+    ),
+    keymap.map_lazy(
+        "n",
+        "<leader>,",
+        keymap.exec("BufferLineMovePrev"),
+        { desc = "Move buffer to previous" }
+    ),
+}
 
 -- go to absolute buffer 1~9, and 0
 for i = 1, 9 do
@@ -19,54 +55,5 @@ for i = 1, 9 do
         )
     )
 end
-table.insert(
-    M,
-    keymap.map_lazy(
-        "n",
-        "<leader>0",
-        keymap.exec("lua require('bufferline').go_to_buffer(-1, true)"),
-        { desc = "Go to the last buffer" }
-    )
-)
-
--- go to next/previous buffer
-table.insert(
-    M,
-    keymap.map_lazy(
-        "n",
-        "]b",
-        keymap.exec("BufferLineCycleNext"),
-        { desc = "Go to next buffer" }
-    )
-)
-table.insert(
-    M,
-    keymap.map_lazy(
-        "n",
-        "[b",
-        keymap.exec("BufferLineCyclePrev"),
-        { desc = "Go to previous buffer" }
-    )
-)
-
--- re-order/move current buffer to next/previous position
-table.insert(
-    M,
-    keymap.map_lazy(
-        "n",
-        "<leader>.",
-        keymap.exec("BufferLineMoveNext"),
-        { desc = "Move buffer to next" }
-    )
-)
-table.insert(
-    M,
-    keymap.map_lazy(
-        "n",
-        "<leader>,",
-        keymap.exec("BufferLineMovePrev"),
-        { desc = "Move buffer to previous" }
-    )
-)
 
 return M
