e62caf5e8dd81307c7f4b2c4811c5c594a585fd6
Rongbin Lin <rlin@flexport.com>
2023-03-02 17:59:02 +0800

    update colors

--- lua/cfg/plugins.lua
+++ lua/cfg/plugins.lua
@@ -26,269 +26,284 @@ return {
         lazy = true,
     },
 
-    -- https://vimcolorschemes.com/
-    {
-        -- https://github.com/nlknguyen/papercolor-theme
-        "nlknguyen/papercolor-theme",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/cocopon/iceberg.vim
-        "cocopon/iceberg.vim",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/junegunn/seoul256.vim
-        "junegunn/seoul256.vim",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/jacoborus/tender.vim
-        "jacoborus/tender.vim",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/lifepillar/vim-solarized8
-        "lifepillar/vim-solarized8",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/jnurmine/zenburn
-        "jnurmine/zenburn",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/romainl/apprentice
-        "romainl/apprentice",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/raphamorim/lucario
-        "raphamorim/lucario",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/srcery-colors/srcery-vim
-        "srcery-colors/srcery-vim",
-        lazy = true,
-        priority = 1000,
-        name = "srcery",
-    },
-    {
-        -- https://github.com/pineapplegiant/spaceduck
-        "pineapplegiant/spaceduck",
-        lazy = true,
-        priority = 1000,
-        branch = "main",
-    },
-    {
-        -- https://github.com/ajmwagar/vim-deus
-        "ajmwagar/vim-deus",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/preservim/vim-colors-pencil
-        "preservim/vim-colors-pencil",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/challenger-deep-theme/vim
-        "challenger-deep-theme/vim",
-        lazy = true,
-        priority = 1000,
-        name = "challenger-deep-theme",
-    },
-    {
-        -- https://github.com/rigellute/rigel
-        "rigellute/rigel",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/lifepillar/vim-gruvbox8
-        "lifepillar/vim-gruvbox8",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/crusoexia/vim-monokai (duplicated)
-        "crusoexia/vim-monokai",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/wadackel/vim-dogrun
-        "wadackel/vim-dogrun",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/jaredgorski/spacecamp
-        "jaredgorski/spacecamp",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/keitanakamura/neodark.vim
-        "keitanakamura/neodark.vim",
-        lazy = true,
-        priority = 1000,
-    },
-
     -- https://www.trackawesomelist.com/rockerBOO/awesome-neovim/readme/#colorscheme
     {
-        -- https://github.com/catppuccin/nvim
-        "catppuccin/nvim",
-        lazy = true,
-        priority = 1000,
-        name = "catppuccin",
-    },
-    {
-        -- https://github.com/rebelot/kanagawa.nvim
-        "rebelot/kanagawa.nvim",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/EdenEast/nightfox.nvim (duplicated)
-        "EdenEast/nightfox.nvim",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/sainnhe/everforest
-        "sainnhe/everforest",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/sainnhe/gruvbox-material
-        "sainnhe/gruvbox-material",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/projekt0n/github-nvim-theme
-        "projekt0n/github-nvim-theme",
-        lazy = true,
-        priority = 1000,
-        branch = "0.0.x",
-    },
-    {
-        -- https://github.com/dracula/vim (duplicated)
-        "dracula/vim",
-        lazy = true,
-        priority = 1000,
-        name = "dracula",
-    },
-    {
-        -- https://github.com/sainnhe/sonokai
-        "sainnhe/sonokai",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/mhartington/oceanic-next
-        "mhartington/oceanic-next",
-        lazy = true,
-        priority = 1000,
-    },
-    {
-        -- https://github.com/folke/tokyonight.nvim (duplicated)
+        -- stars:3200, repo:https://github.com/folke/tokyonight.nvim
         "folke/tokyonight.nvim",
         lazy = true,
         priority = 1000,
         branch = "main",
     },
     {
-        -- https://github.com/ellisonleao/gruvbox.nvim
-        "ellisonleao/gruvbox.nvim",
+        -- stars:2300, repo:https://github.com/catppuccin/nvim
+        "catppuccin/nvim",
+        lazy = true,
+        priority = 1000,
+        name = "catppuccin",
+        branch = "main",
+    },
+    {
+        -- stars:2000, repo:https://github.com/rebelot/kanagawa.nvim
+        "rebelot/kanagawa.nvim",
         lazy = true,
         priority = 1000,
     },
     {
-        -- https://github.com/navarasu/onedark.nvim (duplicated)
+        -- stars:1900, repo:https://github.com/EdenEast/nightfox.nvim
+        "EdenEast/nightfox.nvim",
+        lazy = true,
+        priority = 1000,
+        branch = "main",
+    },
+    {
+        -- stars:1700, repo:https://github.com/sainnhe/everforest
+        "sainnhe/everforest",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1300, repo:https://github.com/sainnhe/gruvbox-material
+        "sainnhe/gruvbox-material",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1300, repo:https://github.com/projekt0n/github-nvim-theme
+        "projekt0n/github-nvim-theme",
+        lazy = true,
+        priority = 1000,
+        branch = "0.0.x",
+    },
+    {
+        -- stars:1200, repo:https://github.com/dracula/vim
+        "dracula/vim",
+        lazy = true,
+        priority = 1000,
+        name = "dracula",
+    },
+    {
+        -- stars:1100, repo:https://github.com/sainnhe/sonokai
+        "sainnhe/sonokai",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1100, repo:https://github.com/mhartington/oceanic-next
+        "mhartington/oceanic-next",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:882, repo:https://github.com/ellisonleao/gruvbox.nvim
+        "ellisonleao/gruvbox.nvim",
+        lazy = true,
+        priority = 1000,
+        branch = "main",
+    },
+    {
+        -- stars:858, repo:https://github.com/navarasu/onedark.nvim
         "navarasu/onedark.nvim",
         lazy = true,
         priority = 1000,
     },
     {
-        -- https://github.com/tomasiser/vim-code-dark
+        -- stars:830, repo:https://github.com/tomasiser/vim-code-dark
         "tomasiser/vim-code-dark",
         lazy = true,
         priority = 1000,
     },
     {
-        -- https://github.com/rose-pine/neovim
+        -- stars:804, repo:https://github.com/rose-pine/neovim
         "rose-pine/neovim",
         lazy = true,
         priority = 1000,
         name = "rose-pine",
+        branch = "main",
         config = function()
             require("rose-pine").setup()
         end,
     },
     {
-        -- https://github.com/marko-cerovac/material.nvim (duplicated)
+        -- stars:717, repo:https://github.com/marko-cerovac/material.nvim
         "marko-cerovac/material.nvim",
         lazy = true,
         priority = 1000,
+        branch = "main",
     },
     {
-        -- https://github.com/sainnhe/edge
+        -- stars:698, repo:https://github.com/sainnhe/edge
         "sainnhe/edge",
         lazy = true,
         priority = 1000,
+        branch = "master",
     },
     {
-        -- https://github.com/fenetikm/falcon
+        -- stars:624, repo:https://github.com/fenetikm/falcon
         "fenetikm/falcon",
         lazy = true,
         priority = 1000,
     },
     {
-        -- https://github.com/nyoom-engineering/oxocarbon.nvim
+        -- stars:580, repo:https://github.com/nyoom-engineering/oxocarbon.nvim
         "nyoom-engineering/oxocarbon.nvim",
         lazy = true,
         priority = 1000,
+        branch = "main",
     },
     {
-        -- https://github.com/bluz71/vim-nightfly-colors
+        -- stars:577, repo:https://github.com/bluz71/vim-nightfly-colors
         "bluz71/vim-nightfly-colors",
         lazy = true,
         priority = 1000,
         name = "nightfly",
     },
     {
-        -- https://github.com/shaunsingh/nord.nvim (duplicated)
+        -- stars:571, repo:https://github.com/shaunsingh/nord.nvim
         "shaunsingh/nord.nvim",
         lazy = true,
         priority = 1000,
     },
     {
-        -- https://github.com/bluz71/vim-moonfly-colors
+        -- stars:556, repo:https://github.com/bluz71/vim-moonfly-colors
         "bluz71/vim-moonfly-colors",
         lazy = true,
         priority = 1000,
         name = "moonfly",
     },
     {
-        -- https://github.com/embark-theme/vim (duplicated)
+        -- stars:527, repo:https://github.com/embark-theme/vim
         "embark-theme/vim",
         lazy = true,
         priority = 1000,
         name = "embark",
         branch = "main",
+    },
+
+    -- https://vimcolorschemes.com/
+    {
+        -- stars:2440, repo:https://github.com/nlknguyen/papercolor-theme
+        "nlknguyen/papercolor-theme",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1976, repo:https://github.com/cocopon/iceberg.vim
+        "cocopon/iceberg.vim",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1561, repo:https://github.com/junegunn/seoul256.vim
+        "junegunn/seoul256.vim",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:1375, repo:https://github.com/sickill/vim-monokai
+        "sickill/vim-monokai",
+        lazy = true,
+        priority = 1000,
+        branch = "master",
+    },
+    {
+        -- stars:1018, repo:https://github.com/jacoborus/tender.vim
+        "jacoborus/tender.vim",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:912, repo:https://github.com/lifepillar/vim-solarized8
+        "lifepillar/vim-solarized8",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:833, repo:https://github.com/jnurmine/zenburn
+        "jnurmine/zenburn",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:826, repo:https://github.com/romainl/apprentice
+        "romainl/apprentice",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:758, repo:https://github.com/raphamorim/lucario
+        "raphamorim/lucario",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:744, repo:https://github.com/srcery-colors/srcery-vim
+        "srcery-colors/srcery-vim",
+        lazy = true,
+        priority = 1000,
+        name = "srcery",
+    },
+    {
+        -- stars:705, repo:https://github.com/pineapplegiant/spaceduck
+        "pineapplegiant/spaceduck",
+        lazy = true,
+        priority = 1000,
+        branch = "main",
+    },
+    {
+        -- stars:673, repo:https://github.com/ajmwagar/vim-deus
+        "ajmwagar/vim-deus",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:576, repo:https://github.com/preservim/vim-colors-pencil
+        "preservim/vim-colors-pencil",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:547, repo:https://github.com/challenger-deep-theme/vim
+        "challenger-deep-theme/vim",
+        lazy = true,
+        priority = 1000,
+        name = "challenger-deep-theme",
+    },
+    {
+        -- stars:488, repo:https://github.com/rigellute/rigel
+        "rigellute/rigel",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:456, repo:https://github.com/lifepillar/vim-gruvbox8
+        "lifepillar/vim-gruvbox8",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:433, repo:https://github.com/wadackel/vim-dogrun
+        "wadackel/vim-dogrun",
+        lazy = true,
+        priority = 1000,
+        branch = "main",
+    },
+    {
+        -- stars:428, repo:https://github.com/jaredgorski/spacecamp
+        "jaredgorski/spacecamp",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:408, repo:https://github.com/keitanakamura/neodark.vim
+        "keitanakamura/neodark.vim",
+        lazy = true,
+        priority = 1000,
+    },
+    {
+        -- stars:400, repo:https://github.com/savq/melange-nvim
+        "savq/melange-nvim",
+        lazy = true,
+        priority = 1000,
     },
 
     -- ---- HIGHLIGHT ----
