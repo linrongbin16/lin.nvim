15fa212402b0bd544821e7512ef62d9150498c32
linrongbin16 <linrongbin16@outlook.com>
2023-03-24 23:36:02 +0800

    plugin: config lazy and mason root dir under ~/.nvim

--- lua/repo/folke/lazy-nvim/config.lua
+++ lua/repo/folke/lazy-nvim/config.lua
@@ -1,6 +1,6 @@
 local const = require("cfg.const")
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.expand('$HOME') .. "/.nvim/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git",
@@ -14,6 +14,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 local opts = {
+    root = vim.fn.expand('$HOME') .. "/.nvim/lazy",
     ui = {
         border = const.ui.border,
     },
--- lua/repo/williamboman/mason-nvim/config.lua
+++ lua/repo/williamboman/mason-nvim/config.lua
@@ -1,6 +1,7 @@
 local const = require("cfg.const")
 
 require("mason").setup({
+    install_root_dir = vim.fn.expand('$HOME') .. "/.nvim/mason",
     ui = {
         border = const.ui.border,
         icons = {
