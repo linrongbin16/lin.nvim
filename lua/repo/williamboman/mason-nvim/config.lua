a75e1a9baa554e4077eac033d66f42df9027b208
linrongbin16 <linrongbin16@outlook.com>
2023-03-27 09:14:11 +0800

    plugin: config lazy and mason root dir under ~/.nvim (#35)
    * plugin: config lazy and mason root dir under ~/.nvim
    
    * use stdpath config
    
    * fix path

--- lua/repo/williamboman/mason-nvim/config.lua
+++ lua/repo/williamboman/mason-nvim/config.lua
@@ -1,6 +1,7 @@
 local const = require("cfg.const")
 
 require("mason").setup({
+    install_root_dir = vim.fn.stdpath("config") .. "/mason",
     ui = {
         border = const.ui.border,
         icons = {
