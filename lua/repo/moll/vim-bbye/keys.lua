c281659c30b2ea8b162c4d01f0147da47b124810
linrongbin16 <linrongbin16@outlook.com>
2023-03-29 18:05:56 +0800

    plugin: add buffer_manager.nvim plugin, rewrite vim-bbye keys (#48)

--- lua/repo/moll/vim-bbye/keys.lua
+++ lua/repo/moll/vim-bbye/keys.lua
@@ -1,25 +1,18 @@
 local keymap = require("cfg.keymap")
 
-local M = {}
-
--- close buffer
-table.insert(
-    M,
+local M = {
     keymap.map_lazy(
         "n",
         "<leader>bd",
         keymap.exec("Bdelete"),
         { desc = "Close buffer" }
-    )
-)
-table.insert(
-    M,
+    ),
     keymap.map_lazy(
         "n",
         "<leader>bD",
         keymap.exec("Bdelete!"),
         { desc = "Close buffer forcibly!" }
-    )
-)
+    ),
+}
 
 return M
