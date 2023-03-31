9e3a578abe6956b136f653c3d252fb0d3fb3b1e0
linrongbin16 <linrongbin16@outlook.com>
2023-04-01 07:30:15 +0800

    save

--- lua/repo/ibhagwan/fzf-lua/keys.lua
+++ lua/repo/ibhagwan/fzf-lua/keys.lua
@@ -21,49 +21,6 @@ local M = {
         end),
         { desc = "Unrestricted(u) search files(f)" }
     ),
-    -- grep
-    keymap.map_lazy(
-        "n",
-        "<space>g",
-        keymap.exec(function()
-            require("fzf-lua").grep()
-        end),
-        { desc = "Grep(g)" }
-    ),
-    keymap.map_lazy(
-        "n",
-        "<space>ug",
-        keymap.exec(function()
-            require("fzf-lua").grep({
-                cmd = fzf_const.GREP_CMD .. " -uu",
-            })
-        end),
-        { desc = "Unrestricted(u) grep(g)" }
-    ),
-    keymap.map_lazy(
-        "n",
-        "<space>og",
-        keymap.exec(function()
-            require("fzf-lua").grep({
-                cmd = fzf_const.GREP_CMD,
-                rg_glob = true,
-                glob_flag = "--glob",
-            })
-        end),
-        { desc = "Glob(o) grep(g)" }
-    ),
-    keymap.map_lazy(
-        "n",
-        "<space>uog",
-        keymap.exec(function()
-            require("fzf-lua").grep({
-                cmd = fzf_const.GREP_CMD .. " -uu",
-                rg_glob = true,
-                glob_flag = "--glob",
-            })
-        end),
-        { desc = "Unrestricted(u) glob(o) grep(g)" }
-    ),
     -- live grep
     keymap.map_lazy(
         "n",
@@ -85,7 +42,7 @@ local M = {
     ),
     keymap.map_lazy(
         "n",
-        "<space>ol",
+        "<space>gl",
         keymap.exec(function()
             require("fzf-lua").live_grep({
                 cmd = fzf_const.GREP_CMD,
@@ -93,11 +50,11 @@ local M = {
                 glob_flag = "--glob",
             })
         end),
-        { desc = "Glob(o) grep(g)" }
+        { desc = "Glob(g) grep(g)" }
     ),
     keymap.map_lazy(
         "n",
-        "<space>uol",
+        "<space>ugl",
         keymap.exec(function()
             require("fzf-lua").live_grep({
                 cmd = fzf_const.GREP_CMD .. " -uu",
@@ -105,7 +62,45 @@ local M = {
                 glob_flag = "--glob",
             })
         end),
-        { desc = "Unrestricted(u) glob(o) live(l) grep" }
+        { desc = "Unrestricted(u) glob(g) live(l) grep" }
+    ),
+    -- grep word
+    keymap.map_lazy(
+        "n",
+        "<space>w",
+        keymap.exec(function()
+            require("fzf-lua").grep_cword()
+        end),
+        { desc = "Grep word(w)" }
+    ),
+    keymap.map_lazy(
+        "n",
+        "<space>uw",
+        keymap.exec(function()
+            require("fzf-lua").grep_cword({
+                cmd = fzf_const.GREP_CMD .. " -uu",
+            })
+        end),
+        { desc = "Unrestricted(u) grep word(w)" }
+    ),
+    -- grep WORD
+    keymap.map_lazy(
+        "n",
+        "<space>W",
+        keymap.exec(function()
+            require("fzf-lua").grep_cWORD()
+        end),
+        { desc = "Grep WORD(W)" }
+    ),
+    keymap.map_lazy(
+        "n",
+        "<space>uw",
+        keymap.exec(function()
+            require("fzf-lua").grep_cWORD({
+                cmd = fzf_const.GREP_CMD .. " -uu",
+            })
+        end),
+        { desc = "Unrestricted(u) grep WORD(W)" }
     ),
     -- FzfLua
     keymap.map_lazy("n", "<Leader>fz", ":FzfLua ", { desc = "Open FzfLua" }),
