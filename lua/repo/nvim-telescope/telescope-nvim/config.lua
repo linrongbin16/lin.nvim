db5a9a0316119abcccb6c9bf01a6972d778725c6
linrongbin16 <linrongbin16@outlook.com>
2023-04-04 11:18:56 +0800

    plugin: use telescope, remove fzf.vim or fzf-lua, to support windows (#59)
    * add telescope
    
    * remove fzf
    
    * save
    
    * save
    
    * update config
    
    * update
    
    * save
    
    * save
    
    * optimize commands
    
    * save
    
    * detect bat

---
+++ lua/repo/nvim-telescope/telescope-nvim/config.lua
@@ -0,0 +1,71 @@
+local const = require("cfg.const")
+local NO_BAT = vim.fn.executable("bat") <= 0
+
+require("telescope").setup({
+    defaults = {
+        layout_config = {
+            preview_width = 0.5,
+            horizontal = {
+                height = 0.9,
+                width = 0.9,
+            },
+            vertical = {
+                height = 0.9,
+                width = 0.9,
+            },
+        },
+        vimgrep_arguments = {
+            "rg",
+            "--color=never",
+            "--no-heading",
+            "-H", -- --with-filename,
+            "-n", -- --line-number
+            "--column",
+            "-S", -- --smart-case,
+        },
+        set_env = {
+            BAT_THEME = "ansi",
+            BAT_STYLE = "numbers,changes",
+        },
+        dynamic_preview_title = true,
+        mappings = {
+            i = {
+                ["<C-s>"] = require("telescope.actions").select_horizontal,
+                ["<C-x>"] = false,
+                ["<C-l>"] = require("telescope.actions.layout").toggle_preview,
+            },
+            n = {
+                ["<C-s>"] = require("telescope.actions").select_horizontal,
+                ["<C-x>"] = false,
+                ["<C-l>"] = require("telescope.actions.layout").toggle_preview,
+            },
+        },
+        file_previewer = (const.os.is_windows or NO_BAT) and require(
+            "telescope.previewers"
+        ).vim_buffer_cat.new or require("telescope.previewers").cat.new,
+        grep_previewer = (const.os.is_windows or NO_BAT) and require(
+            "telescope.previewers"
+        ).vim_buffer_vimgrep.new or require("telescope.previewers").vimgrep.new,
+    },
+    extensions = {
+        fzf = {
+            fuzzy = true,
+            override_generic_sorter = false, -- don't fuzzy on live_grep/grep_string
+            override_file_sorter = true,
+            case_mode = "ignore_case",
+        },
+        live_grep_args = {
+            auto_quoting = true, -- enable/disable auto-quoting
+            mappings = { -- extend mappings
+                i = {
+                    ["<C-k>"] = require("telescope-live-grep-args.actions").quote_prompt(),
+                    ["<C-i>"] = require("telescope-live-grep-args.actions").quote_prompt({
+                        postfix = " --iglob ",
+                    }),
+                },
+            },
+        },
+    },
+})
+
+require("telescope").load_extension("fzf")
