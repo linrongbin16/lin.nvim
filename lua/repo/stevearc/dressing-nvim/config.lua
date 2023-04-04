6ac9530addeb180d5933f34c9d8174b21712131b
linrongbin16 <linrongbin16@outlook.com>
2023-04-04 13:15:16 +0800

    update dressing config

--- lua/repo/stevearc/dressing-nvim/config.lua
+++ lua/repo/stevearc/dressing-nvim/config.lua
@@ -1,8 +1,10 @@
 local const = require("cfg.const")
 local close_mappings =
     { ["<ESC>"] = "Close", ["<C-[>"] = "Close", ["<C-c>"] = "Close" }
-local window_transparency = 0
-local window_min_width = { 50, 0.2 }
+local window_transparency = 10
+local window_min_width_value = 50
+local window_min_width_rate = 0.2
+local window_min_width = { window_min_width_value, window_min_width_rate }
 
 require("dressing").setup({
     input = {
@@ -21,6 +23,10 @@ require("dressing").setup({
             border = {
                 style = const.ui.border,
             },
+            win_options = {
+                winblend = window_transparency,
+            },
+            min_width = window_min_width_value,
         },
         builtin = {
             border = const.ui.border,
