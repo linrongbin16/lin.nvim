282c8a2e9dffb88e1b94f78b81966f12551208f5
linrongbin16 <linrongbin16@outlook.com>
2023-04-04 13:10:10 +0800

    set lazy to telescope

--- lua/cfg/plugins.lua
+++ lua/cfg/plugins.lua
@@ -403,9 +403,11 @@ return {
     {
         "nvim-telescope/telescope-fzf-native.nvim",
         build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+        lazy = true,
     },
     {
         "nvim-telescope/telescope-live-grep-args.nvim",
+        lazy = true,
     },
 
     -- ---- TAGS ----
