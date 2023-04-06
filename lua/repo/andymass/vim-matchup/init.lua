e68254bd630cb4d5f78b891ee70bea25d8917554
linrongbin16 <linrongbin16@outlook.com>
2023-04-06 21:57:10 +0800

    config(ui): defer matchup and add icons

--- lua/repo/andymass/vim-matchup/init.lua
+++ lua/repo/andymass/vim-matchup/init.lua
@@ -2,3 +2,8 @@
 vim.g.loaded_matchit = 1
 -- vim.g.matchup_matchparen_offscreen = { method = "popup" }
 vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
+
+-- defer to better performance
+vim.g.matchup_matchparen_deferred = 1
+vim.g.matchup_matchparen_deferred_show_delay = 50
+vim.g.matchup_matchparen_deferred_hide_delay = 500
