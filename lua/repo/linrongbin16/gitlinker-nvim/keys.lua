e5a1ff270ada8bf9d272e9c5c44a7a719f83746e
linrongbin16 <linrongbin16@outlook.com>
2023-04-20 09:45:55 +0800

    config: use same keys with upstream gitlink

--- lua/repo/linrongbin16/gitlinker-nvim/keys.lua
+++ lua/repo/linrongbin16/gitlinker-nvim/keys.lua
@@ -4,13 +4,13 @@ local M = {
     map_lazy(
         { "n", "x" },
         "<leader>gl",
-        '<cmd>lua require("gitlinker").link()<cr>',
+        '<cmd>lua require("gitlinker").link(action = require("gitlinker.actions").clipboard})<cr>',
         { desc = "Open git link in browser" }
     ),
     map_lazy(
         { "n", "x" },
         "<leader>gL",
-        '<cmd>lua require("gitlinker").link({action = require("gitlinker.actions").clipboard})<cr>',
+        '<cmd>lua require("gitlinker").link({action = require("gitlinker.actions").system})<cr>',
         { desc = "Copy git link to clipboard" }
     ),
 }
