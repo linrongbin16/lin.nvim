d8fc92d69eaf600ac9aac928b10a5eb19ac1aff0
linrongbin16 <linrongbin16@outlook.com>
2023-04-24 13:17:46 +0800

    optimize nvim-cmp setup_handler

--- lua/repo/hrsh7th/nvim-cmp/config.lua
+++ lua/repo/hrsh7th/nvim-cmp/config.lua
@@ -11,97 +11,102 @@ lsp_defaults.capabilities = vim.tbl_deep_extend(
 -- nvim-cmp
 -- require('luasnip.loaders.from_vscode').lazy_load()
 local cmp = require("cmp")
+local luasnip = require("luasnip")
+local select_opts = { behavior = cmp.SelectBehavior.Select }
+local setup_handler = {
+    completion = {
+        completeopt = "menu,menuone,noinsert",
+    },
+    snippet = {
+        expand = function(args)
+            luasnip.lsp_expand(args.body)
+        end,
+    },
+    sources = cmp.config.sources({
+        { name = "nvim_lsp", keyword_length = 2 },
+        { name = "luasnip", keyword_length = 2 },
+    }, {
+        { name = "buffer", keyword_length = 2 },
+        { name = "path", keyword_length = 2 },
+        { name = "tags", keyword_length = 2 },
+    }),
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        -- fields = { "menu", "abbr", "kind" },
+        format = require("lspkind").cmp_format({
+            mode = "symbol",
+            menu = {
+                buffer = "[BUF]",
+                nvim_lsp = "[LSP]",
+                luasnip = "[SNIP]",
+                tags = "[TAGS]",
+                path = "[PATH]",
+                cmdline = "[CMD]",
+                copilot = "[COPILOT]",
+            },
+            symbol_map = { Copilot = "" },
+            maxwidth = 50,
+            ellipsis_char = "…",
+        }),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
+        ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
+        ["<Down>"] = cmp.mapping.select_next_item(select_opts),
+        ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.confirm({ select = true })
+            else
+                -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            local col = vim.fn.col(".") - 1
+            if cmp.visible() then
+                cmp.confirm({ select = true })
+            elseif
+                col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+            then
+                fallback()
+            else
+                cmp.complete()
+            end
+        end, { "i", "s" }),
+        ["<C-f>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(1) then
+                luasnip.jump(1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<C-b>"] = cmp.mapping(function(fallback)
+            if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+    }),
+}
 
-local found_setup_handler, setup_handler =
+local found_user_setup_handler, user_setup_handler =
     pcall(require, "repo.hrsh7th.nvim-cmp.setup_handler")
 
-if not found_setup_handler then
-    local luasnip = require("luasnip")
-    local select_opts = { behavior = cmp.SelectBehavior.Select }
-    setup_handler = {
-        completion = {
-            completeopt = "menu,menuone,noinsert",
-        },
-        snippet = {
-            expand = function(args)
-                luasnip.lsp_expand(args.body)
-            end,
-        },
-        sources = cmp.config.sources({
-            { name = "nvim_lsp", keyword_length = 2 },
-            { name = "luasnip", keyword_length = 2 },
-        }, {
-            { name = "buffer", keyword_length = 2 },
-            { name = "path", keyword_length = 2 },
-            { name = "tags", keyword_length = 2 },
-        }),
-        window = {
-            completion = cmp.config.window.bordered(),
-            documentation = cmp.config.window.bordered(),
-        },
-        formatting = {
-            -- fields = { "menu", "abbr", "kind" },
-            format = require("lspkind").cmp_format({
-                mode = "symbol",
-                menu = {
-                    buffer = "[BUF]",
-                    nvim_lsp = "[LSP]",
-                    luasnip = "[SNIP]",
-                    tags = "[TAGS]",
-                    path = "[PATH]",
-                    cmdline = "[CMD]",
-                    copilot = "[COPILOT]",
-                },
-                symbol_map = { Copilot = "" },
-                maxwidth = 50,
-                ellipsis_char = "…",
-            }),
-        },
-        mapping = cmp.mapping.preset.insert({
-            ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
-            ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
-            ["<Down>"] = cmp.mapping.select_next_item(select_opts),
-            ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
-            ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-d>"] = cmp.mapping.scroll_docs(4),
-            ["<C-Space>"] = cmp.mapping.complete(),
-            ["<C-e>"] = cmp.mapping.abort(),
-            ["<CR>"] = cmp.mapping(function(fallback)
-                if cmp.visible() then
-                    cmp.confirm({ select = true })
-                else
-                    -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
-                    fallback()
-                end
-            end, { "i", "s" }),
-            ["<Tab>"] = cmp.mapping(function(fallback)
-                local col = vim.fn.col(".") - 1
-                if cmp.visible() then
-                    cmp.confirm({ select = true })
-                elseif
-                    col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-                then
-                    fallback()
-                else
-                    cmp.complete()
-                end
-            end, { "i", "s" }),
-            ["<C-f>"] = cmp.mapping(function(fallback)
-                if luasnip.jumpable(1) then
-                    luasnip.jump(1)
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-            ["<C-b>"] = cmp.mapping(function(fallback)
-                if luasnip.jumpable(-1) then
-                    luasnip.jump(-1)
-                else
-                    fallback()
-                end
-            end, { "i", "s" }),
-        }),
-    }
+if found_user_setup_handler then
+    setup_handler = vim.tbl_deep_extend(
+        "force",
+        vim.deepcopy(setup_handler),
+        user_setup_handler
+    )
 end
 
 cmp.setup(setup_handler)
--- lua/repo/hrsh7th/nvim-cmp/setup_handler_sample.lua
+++ lua/repo/hrsh7th/nvim-cmp/setup_handler_sample.lua
@@ -1,17 +1,6 @@
 local cmp = require("cmp")
-local luasnip = require("luasnip")
-
-local select_opts = { behavior = cmp.SelectBehavior.Select }
 
 local setup_handler = {
-    completion = {
-        completeopt = "menu,menuone,noinsert",
-    },
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
     sources = cmp.config.sources({
         { name = "nvim_lsp", keyword_length = 2 },
         { name = "luasnip", keyword_length = 2 },
@@ -38,72 +27,6 @@ local setup_handler = {
             cmp.config.compare.order,
         },
     },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
-    formatting = {
-        -- fields = { "menu", "abbr", "kind" },
-        format = require("lspkind").cmp_format({
-            mode = "symbol",
-            menu = {
-                buffer = "[BUF]",
-                nvim_lsp = "[LSP]",
-                luasnip = "[SNIP]",
-                tags = "[TAGS]",
-                path = "[PATH]",
-                cmdline = "[CMD]",
-                copilot = "[COPILOT]",
-            },
-            symbol_map = { Copilot = "" },
-            maxwidth = 50,
-            ellipsis_char = "…",
-        }),
-    },
-    mapping = cmp.mapping.preset.insert({
-        ["<Up>"] = cmp.mapping.select_prev_item(select_opts),
-        ["<C-p>"] = cmp.mapping.select_prev_item(select_opts),
-        ["<Down>"] = cmp.mapping.select_next_item(select_opts),
-        ["<C-n>"] = cmp.mapping.select_next_item(select_opts),
-        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.confirm({ select = true })
-            else
-                -- If you use vim-endwise, this fallback will behave the same as vim-endwise.
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            local col = vim.fn.col(".") - 1
-            if cmp.visible() then
-                cmp.confirm({ select = true })
-            elseif
-                col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-            then
-                fallback()
-            else
-                cmp.complete()
-            end
-        end, { "i", "s" }),
-        ["<C-f>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(1) then
-                luasnip.jump(1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-        ["<C-b>"] = cmp.mapping(function(fallback)
-            if luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { "i", "s" }),
-    }),
 }
 
 return setup_handler
