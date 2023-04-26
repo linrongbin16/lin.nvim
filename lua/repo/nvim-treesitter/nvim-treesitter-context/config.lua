3e64657b4f8cc7104d31e8701c017853a45fe224
linrongbin16 <linrongbin16@outlook.com>
2023-04-26 09:21:19 +0800

    remove separator

--- lua/repo/nvim-treesitter/nvim-treesitter-context/config.lua
+++ lua/repo/nvim-treesitter/nvim-treesitter-context/config.lua
@@ -1,5 +1,5 @@
 require("treesitter-context").setup({
-    separator = "┄", -- U+2504: https://symbl.cc/en/2504/
+    -- separator = "┄", -- U+2504: https://symbl.cc/en/2504/
     max_lines = 5,
     min_window_height = 10,
 })
