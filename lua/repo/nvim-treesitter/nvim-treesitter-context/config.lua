098b2660e9044a6436d79386d931fc88164f1655
linrongbin16 <linrongbin16@outlook.com>
2023-04-26 09:23:47 +0800

    config: remove separator from nvim treesitter context (#79)
    * update
    
    * remove separator
    
    * set minheight=30

--- lua/repo/nvim-treesitter/nvim-treesitter-context/config.lua
+++ lua/repo/nvim-treesitter/nvim-treesitter-context/config.lua
@@ -1,3 +1,4 @@
 require("treesitter-context").setup({
-    separator = "┄", -- U+2504: https://symbl.cc/en/2504/
+    -- separator = "┄", -- U+2504: https://symbl.cc/en/2504/
+    min_window_height = 30,
 })
