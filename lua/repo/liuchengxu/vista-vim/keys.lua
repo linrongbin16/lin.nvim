9a8b56527e8a5b48d5df01b8b8d67743367acce0
linrongbin16 <linrongbin16@outlook.com>
2023-05-04 09:52:02 +0800

    keymap: remap keys with functionality first words

--- lua/repo/liuchengxu/vista-vim/keys.lua
+++ lua/repo/liuchengxu/vista-vim/keys.lua
@@ -3,9 +3,9 @@ local map_lazy = require("cfg.keymap").map_lazy
 local M = {
     map_lazy(
         "n",
-        "<leader>vt",
+        "<leader>ol",
         ":Vista!!<CR>",
-        { silent = false, desc = "Toggle outlines(tags)" }
+        { silent = false, desc = "Toggle structure outlines(tags)" }
     ),
 }
 
--- lua/repo/nvim-neo-tree/neo-tree-nvim/keys.lua
+++ lua/repo/nvim-neo-tree/neo-tree-nvim/keys.lua
@@ -3,7 +3,7 @@ local map_lazy = require("cfg.keymap").map_lazy
 local M = {
     map_lazy(
         "n",
-        "<leader>nt",
+        "<leader>tr",
         "<cmd>Neotree toggle reveal<cr>",
         { silent = false, desc = "Toggle file explorer" }
     ),
