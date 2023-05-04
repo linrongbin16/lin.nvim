7524842f59dbafaa5b0cea46ce7e8f2dd8da854a
linrongbin16 <linrongbin16@outlook.com>
2023-05-04 10:08:05 +0800

    remap: use \gd instead of \dg for doge-generate

--- lua/repo/kkoomen/vim-doge/keys.lua
+++ lua/repo/kkoomen/vim-doge/keys.lua
@@ -3,7 +3,7 @@ local map_lazy = require("cfg.keymap").map_lazy
 local M = {
     map_lazy(
         "n",
-        "<leader>dg",
+        "<leader>gd",
         ":DogeGenerate ",
         { desc = "Generate document" }
     ),
