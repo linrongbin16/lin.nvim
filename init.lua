645c925c653793083ec9b5137d75dc095a167214
linrongbin16 <linrongbin16@outlook.com>
2023-07-03 17:19:34 +0800

    fix: move lsp to the end

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- basic options
 vim.cmd("source $HOME/.nvim/lua/builtin/options.vim")
-require("builtin.lsp")
 
 -- plugins
 require("configs.folke.lazy-nvim.config")
 
--- colorschemes and other things
+-- other options
+require("builtin.lsp")
 vim.cmd("source $HOME/.nvim/lua/builtin/colors.vim")
 vim.cmd("source $HOME/.nvim/lua/builtin/others.vim")
