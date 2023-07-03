351bd246f9b4ec1f49a8fcfe736a78d7caa7c0c8
linrongbin16 <linrongbin16@outlook.com>
2023-07-03 15:20:36 +0800

    config: update

--- lua/config/neovim/nvim-lspconfig/setup_handlers_sample.lua
+++ lua/config/neovim/nvim-lspconfig/setup_handlers_sample.lua
@@ -1,6 +1,8 @@
 -- Configure extra lspconfig setup handlers.
 -- Only if you cannot find the lsp server or source from mason, such as: `flow`.
 
+-- Please copy this file to 'setup_handlers.lua' to enable it.
+
 local lsp_setup_helper = require("builtin.utils.lsp_setup_helper")
 
 local setup_handlers = {
