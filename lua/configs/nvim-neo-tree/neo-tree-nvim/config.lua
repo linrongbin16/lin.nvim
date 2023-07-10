fd598257f1c8d1b6f390fa2531f02547866e27a4
linrongbin16 <linrongbin16@outlook.com>
2023-07-10 10:00:03 +0800

    fix(neo-tree): nerdfonts v3.x

--- lua/configs/nvim-neo-tree/neo-tree-nvim/config.lua
+++ lua/configs/nvim-neo-tree/neo-tree-nvim/config.lua
@@ -26,6 +26,32 @@ require("neo-tree").setup({
             },
         },
     },
+    document_symbols = {
+        kinds = {
+            File = { icon = "󰈙", hl = "Tag" },
+            Namespace = { icon = "󰌗", hl = "Include" },
+            Package = { icon = "󰏖", hl = "Label" },
+            Class = { icon = "󰌗", hl = "Include" },
+            Property = { icon = "󰆧", hl = "@property" },
+            Enum = { icon = "󰒻", hl = "@number" },
+            Function = { icon = "󰊕", hl = "Function" },
+            String = { icon = "󰀬", hl = "String" },
+            Number = { icon = "󰎠", hl = "Number" },
+            Array = { icon = "󰅪", hl = "Type" },
+            Object = { icon = "󰅩", hl = "Type" },
+            Key = { icon = "󰌋", hl = "" },
+            Struct = { icon = "󰌗", hl = "Type" },
+            Operator = { icon = "󰆕", hl = "Operator" },
+            TypeParameter = { icon = "󰊄", hl = "Type" },
+            StaticMethod = { icon = "󰠄 ", hl = "Function" },
+        },
+    },
+    source_selector = {
+        sources = {
+            { source = "filesystem", display_name = " 󰉓 Files " },
+            { source = "git_status", display_name = " 󰊢 Git " },
+        },
+    },
     renderers = {
         directory = {
             { "indent" },
