997a235c4f3fc988779bee2b8519e3ba2f2427a0
linrongbin16 <linrongbin16@outlook.com>
2023-07-10 16:47:18 +0800

    fix(lualine): lsp progress message (#188)
    * fix(lualine): fix missing lsp message
    
    * fix(noice): disable lsp progress again
    
    * stylish(lualine): remove lsp client count
    
    * stylish(noice): disable split long message

--- lua/configs/folke/noice-nvim/config.lua
+++ lua/configs/folke/noice-nvim/config.lua
@@ -12,12 +12,13 @@ require("noice").setup({
             ["vim.lsp.util.stylize_markdown"] = true,
             ["cmp.entry.get_documentation"] = true,
         },
+        progress = { enabled = false },
     },
     -- you can enable a preset for easier configuration
     presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
+        long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
     },
