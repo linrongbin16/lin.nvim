9057dbca6c0c7b0f6c3bf35cd6d8bebf22d971bb
linrongbin16 <linrongbin16@outlook.com>
2023-07-10 16:47:06 +0800

    stylish(noice): disable split long message

--- lua/configs/folke/noice-nvim/config.lua
+++ lua/configs/folke/noice-nvim/config.lua
@@ -18,7 +18,7 @@ require("noice").setup({
     presets = {
         bottom_search = true, -- use a classic bottom cmdline for search
         command_palette = true, -- position the cmdline and popupmenu together
-        long_message_to_split = true, -- long messages will be sent to a split
+        long_message_to_split = false, -- long messages will be sent to a split
         inc_rename = false, -- enables an input dialog for inc-rename.nvim
         lsp_doc_border = true, -- add a border to hover docs and signature help
     },
