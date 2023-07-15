5d90d895d13375142cfeb9c5b18844ee26b60425
linrongbin16 <linrongbin16@outlook.com>
2023-07-16 05:23:14 +0800

    feat(hooks): add user_init,user_config,user_keys hooks for plugin config

--- lua/builtin/utils/plugin.lua
+++ lua/builtin/utils/plugin.lua
@@ -1,25 +1,28 @@
 local function lua_keys(keys)
+    local user_path = "configs/" .. keys:gsub("%.", "-") .. "/user_keys"
+    local user_ok, user_list = pcall(require, user_path)
+    if user_ok then
+        return user_list
+    end
     local keys_path = "configs/" .. keys:gsub("%.", "-") .. "/keys"
     local keys_list_ok, keys_list = pcall(require, keys_path)
     if not keys_list_ok then
-        error("Error! Lua module '" .. keys_path .. "' not found!")
-    end
-    local user_keys_path = "configs/" .. keys:gsub("%.", "-") .. "/user_keys"
-    local user_keys_list_ok, user_keys_list = pcall(require, user_keys_path)
-    if user_keys_list_ok and type(user_keys_list) == "table" then
-        for _, k in ipairs(user_keys_list) do
-            table.insert(keys_list, k)
-        end
+        error("Error! Failed to load lua module '" .. keys_path .. "'!")
     end
     return keys_list
 end
 
 local function lua_init(init)
     return function()
+        local user_path = "configs/" .. init:gsub("%.", "-") .. "/user_init"
+        local user_ok, user_module = pcall(require, user_path)
+        if user_ok then
+            return user_module
+        end
         local init_path = "configs/" .. init:gsub("%.", "-") .. "/init"
         local ok, init_module = pcall(require, init_path)
         if not ok then
-            error("Error! Lua module '" .. init_path .. "' not found!")
+            error("Error! Failed to load lua module '" .. init_path .. "'!")
         end
         return init_module
     end
@@ -27,23 +30,36 @@ end
 
 local function vim_init(init)
     return function()
+        local user_path = vim.fn.stdpath("config")
+            .. "/lua/configs/"
+            .. init:gsub("%.", "-")
+            .. "/user_init.vim"
+        if vim.fn.filereadable(user_path) > 0 then
+            vim.cmd([[source ]] .. user_path)
+            return
+        end
         local init_path = vim.fn.stdpath("config")
             .. "/lua/configs/"
             .. init:gsub("%.", "-")
             .. "/init.vim"
-        if not vim.fn.filereadable(vim.fn.expand(init_path)) then
-            error("Error! Vimscript '" .. init_path .. "' not found!")
+        if vim.fn.filereadable(init_path) <= 0 then
+            error("Error! Failed to load vimscript '" .. init_path .. "'!")
         end
-        vim.cmd("source " .. init_path)
+        vim.cmd([[source ]] .. init_path)
     end
 end
 
 local function lua_config(config)
     return function()
+        local user_path = "configs/" .. init:gsub("%.", "-") .. "/user_config"
+        local user_ok, user_module = pcall(require, user_path)
+        if user_ok then
+            return user_module
+        end
         local config_path = "configs/" .. config:gsub("%.", "-") .. "/config"
         local ok, config_module = pcall(require, config_path)
         if not ok then
-            error("Error! Lua module '" .. config_path .. "' not found!")
+            error("Error! Failed to load lua module '" .. config_path .. "'!")
         end
         return config_module
     end
@@ -51,12 +67,20 @@ end
 
 local function vim_config(config)
     return function()
+        local user_path = vim.fn.stdpath("config")
+            .. "/lua/configs/"
+            .. init:gsub("%.", "-")
+            .. "/user_config.vim"
+        if vim.fn.filereadable(user_path) > 0 then
+            vim.cmd([[source ]] .. user_path)
+            return
+        end
         local config_path = vim.fn.stdpath("config")
             .. "/lua/configs/"
             .. config:gsub("%.", "-")
             .. "/config.vim"
-        if not vim.fn.filereadable(vim.fn.expand(config_path)) then
-            error("Error! Vimscript '" .. config_path .. "' not found!")
+        if vim.fn.filereadable(config_path) <= 0 then
+            error("Error! Failed to load vimscript '" .. config_path .. "'!")
         end
         vim.cmd("source " .. config_path)
     end
