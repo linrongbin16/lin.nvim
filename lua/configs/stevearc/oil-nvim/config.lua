d1a41aca15f6ddc807e31e51c7c520ff8b893d11
linrongbin16 <linrongbin16@outlook.com>
2023-07-18 18:11:31 +0800

    feat(oil): add oil.nvim

---
+++ lua/configs/stevearc/oil-nvim/config.lua
@@ -0,0 +1,47 @@
+-- ["d"] = vim.fn.executable("trash") > 0 and trash_bin or "delete",
+
+local constants = require("builtin.utils.constants")
+
+require("oil").setup({
+    columns = {
+        "icon",
+        "permissions",
+        "size",
+        "mtime",
+    },
+    delete_to_trash = vim.fn.executable("trash") > 0,
+    trash_command = "trash",
+    keymaps = {
+        -- open in new tab/split/vsplit
+        ["<C-w>v"] = "actions.select_vsplit",
+        ["<C-s>"] = false,
+        ["<C-w>s"] = "actions.select_split",
+        ["<C-h>"] = false,
+        ["<C-w>t"] = "actions.select_tab",
+        ["<C-t>"] = false,
+
+        -- preview, refresh
+        ["<C-l>"] = "actions.preview",
+        ["<C-p>"] = false,
+        ["<C-r>"] = "actions.refresh",
+
+        -- go to upper folder
+        ["<BS>"] = "actions.parent",
+        ["-"] = false,
+    },
+    view_options = {
+        show_hidden = true,
+    },
+    float = {
+        border = constants.ui.border,
+        win_options = {
+            winblend = constants.ui.winblend,
+        },
+    },
+    preview = {
+        border = constants.ui.border,
+    },
+    progress = {
+        border = constants.ui.border,
+    },
+})
