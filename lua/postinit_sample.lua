96c49fe141733476b6cdd35efe6cd3f796b8c306
linrongbin16 <linrongbin16@outlook.com>
2023-07-18 11:02:36 +0800

    break(mswin): move ctrl/cmd-? keys to user sample

--- lua/postinit_sample.lua
+++ lua/postinit_sample.lua
@@ -1,1 +1,11 @@
 -- Please copy this file to 'postinit.lua' to enable it.
+
+local constants = require("builtin.utils.constants")
+
+-- ctrl/cmd-?
+if constants.os.is_windows and vim.fn.exists("$VIMRUNTIME/mswin.vim") > 0 then
+    vim.cmd([[source $VIMRUNTIME/mswin.vim]])
+end
+if constants.os.is_macos and vim.fn.exists("$VIMRUNTIME/macmap.vim") > 0 then
+    vim.cmd([[source $VIMRUNTIME/macmap.vim]])
+end
