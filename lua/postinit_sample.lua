4b26cd25c82cf2273d15909b62398dfbb8f72569
linrongbin16 <linrongbin16@outlook.com>
2023-07-18 17:19:13 +0800

    revert(mswin): enable mswin for all platforms

--- lua/postinit_sample.lua
+++ lua/postinit_sample.lua
@@ -3,7 +3,7 @@
 local constants = require("builtin.utils.constants")
 
 -- ctrl/cmd-?
-if constants.os.is_windows and vim.fn.exists("$VIMRUNTIME/mswin.vim") > 0 then
+if vim.fn.exists("$VIMRUNTIME/mswin.vim") > 0 then
     vim.cmd([[source $VIMRUNTIME/mswin.vim]])
 end
 if constants.os.is_macos and vim.fn.exists("$VIMRUNTIME/macmap.vim") > 0 then
