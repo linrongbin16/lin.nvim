00911dc571e02904d3a1b800ced8905f454bc685
linrongbin16 <linrongbin16@outlook.com>
2023-07-20 10:24:10 +0800

    perf(mason-null-ls): auto setup

--- lua/configs/williamboman/mason-lspconfig-nvim/config.lua
+++ lua/configs/williamboman/mason-lspconfig-nvim/config.lua
@@ -1,11 +1,24 @@
 local lspconfig = require("lspconfig")
 local message = require("builtin.utils.message")
 
-local ensure_installed_ok, ensure_installed =
-    pcall(require, "configs.williamboman.mason-lspconfig-nvim.ensure_installed")
+local ensure_installed = {}
 
-if not ensure_installed_ok then
-    ensure_installed = {}
+local user_ensure_installed_module =
+    "configs.williamboman.mason-lspconfig-nvim.ensure_installed"
+local user_ensure_installed_ok, user_ensure_installed =
+    pcall(require, user_ensure_installed_module)
+
+if user_ensure_installed_ok then
+    if type(user_ensure_installed) == "table" then
+        ensure_installed = user_ensure_installed
+    else
+        message.warn(
+            string.format(
+                "Error loading '%s' lua module!",
+                user_ensure_installed_module
+            )
+        )
+    end
 end
 
 require("mason-lspconfig").setup({ ensure_installed = ensure_installed })
