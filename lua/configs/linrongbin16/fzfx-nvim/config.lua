abf5ad3f5c8a88617c69b7f9530d76f0a6db9f6c
linrongbin16 <linrongbin16@outlook.com>
2023-09-19 09:30:28 +0800

    fix

--- lua/configs/linrongbin16/fzfx-nvim/config.lua
+++ lua/configs/linrongbin16/fzfx-nvim/config.lua
@@ -1,10 +1,6 @@
 local constants = require("builtin.utils.constants")
 
 require("fzfx").setup({
-    debug = {
-        enable = true,
-        file_log = true,
-    },
     popup = {
         win_opts = {
             height = constants.ui.layout.middle.scale,
