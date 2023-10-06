c91616190ef2f38385379a97573782393ea2f850
linrongbin16 <linrongbin16@outlook.com>
2023-10-06 22:22:24 +0800

    perf(lsp-progress.nvim): use pattern for lsp-progress event (#264)

--- lua/configs/nvim-lualine/lualine-nvim/config.lua
+++ lua/configs/nvim-lualine/lualine-nvim/config.lua
@@ -152,7 +152,8 @@ require("lualine").setup(config)
 
 -- listen to lsp-progress event and refresh
 vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
-vim.api.nvim_create_autocmd("User LspProgressStatusUpdated", {
+vim.api.nvim_create_autocmd("User", {
     group = "lualine_augroup",
+    pattern = "LspProgressStatusUpdated",
     callback = require("lualine").refresh,
 })
