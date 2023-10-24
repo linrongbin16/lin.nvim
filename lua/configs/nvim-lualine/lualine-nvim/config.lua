4f0692281fe9f682a0488f0410ea83c432c07531
linrongbin16 <linrongbin16@outlook.com>
2023-10-24 10:59:18 +0800

    perf(lualine.nvim): remove deadcode (#320)
    * perf(lualine.nvim): better style
    
    * perf(lualine.nvim): remove deadcode
    
    * perf(lualine.nvim): fix styla

--- lua/configs/nvim-lualine/lualine-nvim/config.lua
+++ lua/configs/nvim-lualine/lualine-nvim/config.lua
@@ -1,67 +1,12 @@
--- local function Rtrim(s)
---     local n = #s
---     while n > 0 and s:find("^%s", n) do
---         n = n - 1
---     end
---     return s:sub(1, n)
--- end
-
 local function GitDiff()
     if vim.g.loaded_gitgutter <= 0 then
-        return ""
+        return {}
     end
     local changes = vim.fn["GitGutterGetHunkSummary"]()
     if changes == nil or #changes ~= 3 then
-        return ""
+        return {}
     end
-    -- added, modified, removed
-    local signs = { "+", "~", "-" }
-    local msg = {}
-    for i = 1, 3 do
-        if changes[i] > 0 then
-            table.insert(msg, signs[i] .. changes[i])
-        end
-    end
-    return #msg > 0 and table.concat(msg, " ") or ""
-end
-
--- local function CursorHex()
---     return "0x%04B"
--- end
-
-local function Ctags()
-    if vim.g.loaded_gutentags <= 0 then
-        return ""
-    end
-    local stats = vim.fn["gutentags#statusline"]()
-    return (stats == nil or stats == "") and ""
-        or string.format("♨ %s", stats)
-end
-
-local function LspIcon()
-    local active_clients_count = #vim.lsp.get_active_clients()
-    return active_clients_count > 0 and " LSP" or ""
-end
-
-local function LspStatus()
-    return require("lsp-progress").progress({
-        format = function(messages)
-            return #messages > 0 and table.concat(messages, " ") or ""
-        end,
-    })
-end
-
-local function MatchUp()
-    if
-        vim.g.loaded_matchup > 0
-        and vim.g.matchup_matchparen_offscreen["method"] == "status_manual"
-    then
-        local status = vim.fn["MatchupStatusOffscreen"]()
-        if status ~= nil and string.len(status) > 0 then
-            return "Δ " .. status
-        end
-    end
-    return ""
+    return { added = changes[1], modified = changes[2], removed = changes[3] }
 end
 
 local function Search()
@@ -94,7 +39,7 @@ local angle_component_separators = { left = "", right = "" }
 local angle_section_separators = { left = "", right = "" }
 
 -- style-2: A \ B \ C ---- X / Y / Z
-local slash_component_separators = { left = "", right = "" } -- nf-ple-backslash_separator \ue0b9, nf-ple-forwardslash_separator \ue0bb
+local slash_component_separators = { left = "", right = "" }
 local slash_section_separators = { left = "", right = "" }
 
 local config = {
@@ -107,7 +52,10 @@ local config = {
         lualine_a = { "mode" },
         lualine_b = {
             "branch",
-            GitDiff,
+            {
+                "diff",
+                source = GitDiff,
+            },
         },
         lualine_c = {
             "filename",
@@ -120,12 +68,9 @@ local config = {
                     hint = constants.diagnostic.sign.hint .. " ",
                 },
             },
-            LspIcon,
-            LspStatus,
-            Ctags,
+            require("lsp-progress").progress,
         },
         lualine_x = {
-            MatchUp,
             Search,
             "filetype",
             {
