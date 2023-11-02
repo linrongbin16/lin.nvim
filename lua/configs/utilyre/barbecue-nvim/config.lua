9ee9cf8cf62b441f73e8504d1b2458cda666e8fa
linrongbin16 <linrongbin16@outlook.com>
2023-11-02 09:16:40 +0800

    perf(barbecue.nvim): set show_modified=false

--- lua/configs/utilyre/barbecue-nvim/config.lua
+++ lua/configs/utilyre/barbecue-nvim/config.lua
@@ -1,11 +1,9 @@
 require("barbecue").setup({
-    -- attach_navic = false, -- prevent auto attach, do it manually
     symbols = {
         separator = "", -- nf-oct-chevron_right \uf460
     },
     -- performance
     create_autocmd = false,
-    show_modified = true,
 })
 
 vim.api.nvim_create_augroup("barbecue_augroup", { clear = true })
@@ -15,11 +13,9 @@ vim.api.nvim_create_autocmd({
     "BufWinEnter",
     "CursorHold",
     "InsertLeave",
-    -- include these if you have set `show_modified` to `true`
-    "BufModifiedSet",
 }, {
     group = "barbecue_augroup",
-    callback = function(data)
+    callback = function()
         require("barbecue.ui").update()
     end,
 })
