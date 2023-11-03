f4750a2963d46038aafa595fcd3d8708d14b497f
linrongbin16 <linrongbin16@outlook.com>
2023-11-03 08:58:00 +0800

    perf(conform.nvim): support biome formatters

--- lua/configs/stevearc/conform-nvim/formatters_by_ft_sample.lua
+++ lua/configs/stevearc/conform-nvim/formatters_by_ft_sample.lua
@@ -3,25 +3,27 @@
 -- Configure formatters by filetypes.
 -- This module will be passed to `require("conform").setup({ formatters_by_ft = ... })`.
 
+local biome_or_prettier_d = { "biome", "prettierd", "prettier" }
+
 local formatters_by_ft = {
     bash = { "shfmt" },
     c = { "clang_format" },
-    css = { "prettier" },
+    css = { biome_or_prettier_d },
     cpp = { "clang_format" },
     cmake = { "cmake_format" },
-    html = { "prettier" },
-    javascript = { "prettier" },
-    javascriptreact = { "prettier" },
-    json = { "prettier" },
-    jsonc = { "prettier" },
+    html = { biome_or_prettier_d },
+    javascript = { biome_or_prettier_d },
+    javascriptreact = { biome_or_prettier_d },
+    json = { biome_or_prettier_d },
+    jsonc = { biome_or_prettier_d },
     lua = { "stylua" },
-    markdown = { "prettier" },
+    markdown = { biome_or_prettier_d },
     python = { "isort", "black" },
     sh = { "shfmt" },
-    typescript = { "prettier" },
-    typescriptreact = { "prettier" },
-    xhtml = { "prettier" },
-    xml = { "prettier" },
+    typescript = { biome_or_prettier_d },
+    typescriptreact = { biome_or_prettier_d },
+    xhtml = { biome_or_prettier_d },
+    xml = { biome_or_prettier_d },
     rust = { "rustfmt" },
 }
 
