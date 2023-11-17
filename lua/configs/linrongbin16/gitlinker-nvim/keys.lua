af82a4420544090409cfc622e0e9d6a5000e8425
linrongbin16 <linrongbin16@outlook.com>
2023-11-17 10:03:17 +0800

    chore(gitlinker.nvim): upgrade to 'GitLink' command (#369)

--- lua/configs/linrongbin16/gitlinker-nvim/keys.lua
+++ lua/configs/linrongbin16/gitlinker-nvim/keys.lua
@@ -1,16 +1,18 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-    set_lazy_key({ "n", "x" }, "<leader>gl", function()
-        require("gitlinker").link({
-            action = require("gitlinker.actions").clipboard,
-        })
-    end, { desc = "Copy git link to clipboard" }),
-    set_lazy_key({ "n", "x" }, "<leader>gL", function()
-        require("gitlinker").link({
-            action = require("gitlinker.actions").system,
-        })
-    end, { desc = "Open git link in browser" }),
+    set_lazy_key(
+        { "n", "x" },
+        "<leader>gl",
+        "<cmd>GitLink<cr>",
+        { desc = "Copy git link to clipboard" }
+    ),
+    set_lazy_key(
+        { "n", "x" },
+        "<leader>gL",
+        "<cmd>GitLink!<cr>",
+        { desc = "Open git link in browser" }
+    ),
 }
 
 return M
