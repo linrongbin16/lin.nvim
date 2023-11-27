46a064709e825bf9856f3b9444b6c12daff21ec4
linrongbin16 <linrongbin16@outlook.com>
2023-11-27 15:05:25 +0800

    chore

--- lua/configs/stevearc/stickybuf-nvim/config.lua
+++ lua/configs/stevearc/stickybuf-nvim/config.lua
@@ -6,21 +6,25 @@ vim.schedule(function()
         group = "stickybuf_augroup",
         callback = function()
             vim.schedule(function()
-                local bufnrs_list = vim.api.nvim_list_bufs()
-                local bufnrs_count = 0
-                if type(bufnrs_list) == "table" then
-                    for _, bufnr in ipairs(bufnrs_list) do
+                local bufnrs = vim.api.nvim_list_bufs()
+                local bufs_count = 0
+                local del_count = 0
+                local del_bufs = {}
+                if type(bufnrs) == "table" then
+                    for _, bufnr in ipairs(bufnrs) do
+                        local bufname = vim.api.nvim_buf_get_name(bufnr)
                         if not require("stickybuf").should_auto_pin(bufnr) then
-                            bufnrs_count = bufnrs_count + 1
+                            bufs_count = bufs_count + 1
+                        end
+                        if string.len(bufname) == 0 then
+                            del_count = del_count + 1
+                            table.insert(del_bufs, bufnr)
                         end
                     end
                 end
-                if type(bufnrs_list) == "table" then
-                    for i, bufnr in ipairs(bufnrs_list) do
-                        local bufname = vim.api.nvim_buf_get_name(bufnr)
-                        if string.len(bufname) == 0 and bufnrs_count > 2 then
-                            vim.api.nvim_buf_delete(bufnr, {})
-                        end
+                if bufs_count > del_count + 1 then
+                    for _, bufnr in ipairs(del_bufs) do
+                        vim.api.nvim_buf_delete(bufnr, {})
                     end
                 end
             end)
