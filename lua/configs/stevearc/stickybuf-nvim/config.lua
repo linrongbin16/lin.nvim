39fa5c99a879756f2f8133e65c19fdf9784d11a9
linrongbin16 <linrongbin16@outlook.com>
2023-11-29 14:47:01 +0800

    fix(stickybuf.nvim): fix auto close 'No Name' buffer

--- lua/configs/stevearc/stickybuf-nvim/config.lua
+++ lua/configs/stevearc/stickybuf-nvim/config.lua
@@ -4,31 +4,31 @@ vim.api.nvim_create_augroup("stickybuf_augroup", { clear = true })
 vim.api.nvim_create_autocmd({ "BufNewFile", "BufReadPost" }, {
     group = "stickybuf_augroup",
     callback = function()
-        vim.schedule(function()
+        vim.defer_fn(function()
             local bufnrs = vim.api.nvim_list_bufs()
-            local bufs_count = 0
-            local del_bufnr = nil
-            local del_count = 0
+            local totals = 0
+            local targets = 0
+            local target_bufnr = nil
             if type(bufnrs) == "table" then
-                for _, bufnr in ipairs(bufnrs) do
-                    local bufname = vim.api.nvim_buf_get_name(bufnr)
-                    if not require("stickybuf").should_auto_pin(bufnr) then
-                        bufs_count = bufs_count + 1
+                for _, bn in ipairs(bufnrs) do
+                    local bufname = vim.api.nvim_buf_get_name(bn)
+                    if not require("stickybuf").should_auto_pin(bn) then
+                        totals = totals + 1
                     end
                     if string.len(bufname) == 0 then
-                        del_count = del_count + 1
-                        del_bufnr = bufnr
+                        targets = targets + 1
+                        target_bufnr = bn
                     end
                 end
             end
             if
-                bufs_count > 0
-                and del_count > 0
-                and bufs_count > del_count + 1
-                and del_bufnr ~= nil
+                totals > 0
+                and targets > 0
+                and totals > targets
+                and target_bufnr ~= nil
             then
-                vim.api.nvim_buf_delete(del_bufnr, {})
+                vim.api.nvim_buf_delete(target_bufnr, {})
             end
-        end)
+        end, 1000)
     end,
 })
