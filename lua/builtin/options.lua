ca3f7a00300bab9f63dabc7ce1e038cce229f216
linrongbin16 <linrongbin16@outlook.com>
2023-11-30 09:36:45 +0800

    perf(options): enable 'exrc' option

--- lua/builtin/options.lua
+++ lua/builtin/options.lua
@@ -10,7 +10,7 @@ vim.opt.whichwrap:append("b,s,<,>,[,]")
 -- complete option
 vim.opt.completeopt:append("menu,menuone,preview")
 
--- indent with 4 spaces
+-- indent
 vim.opt.cindent = true
 vim.opt.smartindent = true
 vim.opt.autoindent = true
@@ -19,6 +19,9 @@ vim.opt.smarttab = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+
+vim.g.editorconfig = true
+vim.opt.exrc = true
 
 -- clipboard with system
 vim.opt.clipboard:prepend("unnamedplus")
