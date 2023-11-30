ff1c0f69f3a332833d274b287cf0a1d1db8690f2
linrongbin16 <linrongbin16@outlook.com>
2023-11-30 09:58:00 +0800

    chore

--- lua/configs/nvim-lualine/lualine-nvim/config.lua
+++ lua/configs/nvim-lualine/lualine-nvim/config.lua
@@ -107,5 +107,5 @@ vim.api.nvim_create_augroup("lualine_augroup", { clear = true })
 vim.api.nvim_create_autocmd("User", {
     group = "lualine_augroup",
     pattern = "LspProgressStatusUpdated",
-    callback = require("lualine").refresh,
+    callback = require("lualine").refresh({ place = { "statusline" } }),
 })
