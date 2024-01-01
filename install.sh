500f1dba36f2cf7e2daac15660c1f58bad4f8c5a
linrongbin16 <linrongbin16@outlook.com>
2024-01-01 14:23:58 +0800

    chore

--- install.sh
+++ install.sh
@@ -275,6 +275,54 @@ install_dnf() {
 
 # }
 
+# packman: manjaro/archlinux {
+
+install_pacman() {
+    message "install dependencies with pacman"
+    sudo pacman -Syy
+
+    # neovim
+    install "yes | sudo pacman -S neovim" "nvim"
+
+    # c++ toolchain
+    install "yes | sudo pacman -S base-devel" "gcc"
+    install "yes | sudo pacman -S base-devel" "make"
+    install "yes | sudo pacman -S autoconf" "autoconf"
+    install "yes | sudo pacman -S automake" "automake"
+    install "yes | sudo pacman -S pkg-config" "pkg-config"
+    install "yes | sudo pacman -S cmake" "cmake"
+
+    # download tools
+    install "yes | sudo pacman -S git" "git"
+    install "yes | sudo pacman -S curl" "curl"
+    install "yes | sudo pacman -S wget" "wget"
+
+    # compress tools
+    install "yes | sudo pacman -S gzip" "gzip"
+    install "yes | sudo pacman -S p7zip" "7z"
+    install "yes | sudo pacman -S unzip" "unzip"
+    install "yes | sudo pacman -S unrar" "unrar"
+
+    # luarocks
+    install "yes | sudo pacman -S luarocks" "luarocks"
+
+    # copy/paste tools
+    install "yes | sudo pacman -S xsel" "xsel"
+    install "yes | sudo pacman -S xclip" "xclip"
+
+    # python3
+    install "yes | sudo pacman -S python python-pip" "python3"
+    install "yes | sudo pacman -S python python-pip" "pip3"
+
+    # node
+    install "yes | sudo pacman -S nodejs npm" "node"
+
+    # ctags
+    install "yes | sudo pacman -S ctags" "ctags"
+}
+
+# }
+
 # dependency
 
 rust_dependency() {
