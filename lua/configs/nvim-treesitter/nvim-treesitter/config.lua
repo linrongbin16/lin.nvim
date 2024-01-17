74b750a3b9133a83b28296a49fced64f46fffc07
linrongbin16 <linrongbin16@outlook.com>
2024-01-17 12:42:52 +0800

    chore

--- lua/configs/nvim-treesitter/nvim-treesitter/config.lua
+++ lua/configs/nvim-treesitter/nvim-treesitter/config.lua
@@ -1,5 +1,6 @@
 local constants = require("builtin.utils.constants")
 local message = require("builtin.utils.message")
+local uv = vim.uv or vim.loop
 
 local ensure_installed = {
     "vim",
@@ -57,8 +58,7 @@ require("nvim-treesitter.configs").setup({
         -- disable for super large file
         disable = function(lang, buf)
             local max_filesize = constants.perf.file.maxsize
-            local ok, stats =
-                pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            local ok, stats = pcall(uv.fs_stat, vim.api.nvim_buf_get_name(buf))
             if ok and stats and stats.size > max_filesize then
                 return true
             end
