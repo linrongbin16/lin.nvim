d3b779fcf2d8c1b6eab40a1fc4ad664fa9bc280b
linrongbin16 <linrongbin16@outlook.com>
2024-01-19 14:26:43 +0800

    perf(nvim-colorizer.lua): enable all colors (#450)

--- lua/configs/NvChad/nvim-colorizer-lua/config.lua
+++ lua/configs/NvChad/nvim-colorizer-lua/config.lua
@@ -1,4 +1,10 @@
 require("colorizer").setup({
+    RRGGBBAA = true,
+    AARRGGBB = true,
+    rgb_fn = true,
+    hsl_fn = true,
     css = true,
     css_fn = true,
+    tailwind = true,
+    sass = { enable = true },
 })
