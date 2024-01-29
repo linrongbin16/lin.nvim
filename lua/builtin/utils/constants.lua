786775bb436d62c8927da65b9da65c9b58b40432
linrongbin16 <linrongbin16@outlook.com>
2024-01-29 09:48:58 +0800

    fix(vim-illuminate): fix hi trigger event (#452)
    feat(neo-tree.nvim): auto-resize file explorer sidebar width (#452)

--- lua/builtin/utils/constants.lua
+++ lua/builtin/utils/constants.lua
@@ -1,61 +1,37 @@
 -- ---- Constants ----
 
---- @type string
 local OS = vim.loop.os_uname().sysname
 
 local M = {
-    --- @type table<string, any>
     diagnostic = {
-        --- @type table<string, any>
         sign = {
-            --- @type string
             error = "", -- nf-fa-times \uf00d
-            --- @type string
             warning = "", -- nf-fa-warning \uf071
-            --- @type string
             info = "", -- nf-fa-info_circle \uf05a
-            --- @type string
             hint = "", -- nf-fa-bell \uf0f3
-            --- @type string
             ok = "", -- nf-fa-check \uf00c
         },
     },
-    --- @type table<string, any>
     ui = {
-        --- @type string
         border = "rounded", -- single,double,rounded,solid,shadow
-        --- @type integer
         winblend = 15,
-        --- @type integer
         pumblend = 15,
-        --- @type table<string, any>
         layout = {
-            --- @type table<string, number>
             middle = {
-                --- @type number
                 scale = 0.85,
             },
-            --- @type table<string, number>
             large = {
-                --- @type number
                 scale = 0.9,
             },
-            --- @type table<string, number>
             sidebar = {
-                --- @type number
                 scale = 0.2,
-                --- @type number
                 min = 20,
-                --- @type number
                 max = 60,
             },
         },
     },
-    --- @type table<string, any>
     perf = {
-        --- @type table<string, any>
         file = {
-            --- @type integer
             maxsize = 1024 * 1024 * 5, -- 5MB
         },
     },
--- lua/configs/RRethy/vim-illuminate/config.lua
+++ lua/configs/RRethy/vim-illuminate/config.lua
@@ -2,15 +2,16 @@ local constants = require("builtin.utils.constants")
 
 require("illuminate").configure({
     -- delay: delay in milliseconds
-    delay = 500,
+    delay = 300,
     -- disable cursor word for big file
     large_file_cutoff = constants.perf.file.maxsize,
 })
 
 -- highlight style
-vim.cmd([[
-augroup vim_illuminate_augroup
-    autocmd!
-    autocmd VimEnter * hi illuminatedWord cterm=underline gui=underline
-augroup END
-]])
+vim.api.nvim_create_augroup("vim_illuminate_augroup", { clear = true })
+vim.api.nvim_create_autocmd({ "UIEnter", "BufReadPre", "BufNewFile" }, {
+    group = "vim_illuminate_augroup",
+    callback = function()
+        vim.cmd([[hi illuminatedWord cterm=underline gui=underline]])
+    end,
+})
