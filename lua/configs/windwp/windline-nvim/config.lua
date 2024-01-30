5b97968000d2faa4f44824794eb711db19e5b9f2
linrongbin16 <linrongbin16@outlook.com>
2024-01-30 13:10:20 +0800

    chore

--- lua/configs/windwp/windline-nvim/config.lua
+++ lua/configs/windwp/windline-nvim/config.lua
@@ -551,6 +551,9 @@ windline.setup({
             "Comment",
         }) or colors.blue
 
+        colors.white = hl_color({ "Normal" }) or "white"
+        colors.black = hl_color({ "Normal" }, "bg") or "black"
+
         return colors
     end,
     statuslines = {
