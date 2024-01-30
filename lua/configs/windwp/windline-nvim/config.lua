728f61b7df98a23b010914877055c182c3d78d4a
linrongbin16 <linrongbin16@outlook.com>
2024-01-30 13:48:51 +0800

    chore

--- lua/configs/windwp/windline-nvim/config.lua
+++ lua/configs/windwp/windline-nvim/config.lua
@@ -514,7 +514,7 @@ windline.setup({
 
         local factors = { 1, 0.5, 0.65, 0.8 }
 
-        colors.normal = hl_color({ "Special" }) or colors.magenta
+        colors.normal = hl_color({ "Directory" }) or colors.magenta
         def("normal", factors)
 
         colors.insert = hl_color({ "GitGutterAdd", "diffAdded", "DiffAdd" })
