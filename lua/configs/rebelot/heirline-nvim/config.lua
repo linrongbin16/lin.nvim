af01f5763095f789158b013481f0047bf22ecee1
linrongbin16 <linrongbin16@outlook.com>
2024-02-08 08:40:26 +0800

    chore

---
+++ lua/configs/rebelot/heirline-nvim/config.lua
@@ -0,0 +1,107 @@
+local conditions = require("heirline.conditions")
+local utils = require("heirline.utils")
+
+local colors = {
+    bright_bg = utils.get_highlight("Folded").bg,
+    bright_fg = utils.get_highlight("Folded").fg,
+    red = utils.get_highlight("DiagnosticError").fg,
+    dark_red = utils.get_highlight("DiffDelete").bg,
+    green = utils.get_highlight("String").fg,
+    blue = utils.get_highlight("Function").fg,
+    gray = utils.get_highlight("NonText").fg,
+    orange = utils.get_highlight("Constant").fg,
+    purple = utils.get_highlight("Statement").fg,
+    cyan = utils.get_highlight("Special").fg,
+    diag_warn = utils.get_highlight("DiagnosticWarn").fg,
+    diag_error = utils.get_highlight("DiagnosticError").fg,
+    diag_hint = utils.get_highlight("DiagnosticHint").fg,
+    diag_info = utils.get_highlight("DiagnosticInfo").fg,
+    git_del = utils.get_highlight("diffDeleted").fg,
+    git_add = utils.get_highlight("diffAdded").fg,
+    git_change = utils.get_highlight("diffChanged").fg,
+}
+
+local ModeNames = {
+    ["n"] = "NORMAL",
+    ["no"] = "O-PENDING",
+    ["nov"] = "O-PENDING",
+    ["noV"] = "O-PENDING",
+    ["no\22"] = "O-PENDING",
+    ["niI"] = "NORMAL",
+    ["niR"] = "NORMAL",
+    ["niV"] = "NORMAL",
+    ["nt"] = "NORMAL",
+    ["ntT"] = "NORMAL",
+    ["v"] = "VISUAL",
+    ["vs"] = "VISUAL",
+    ["V"] = "V-LINE",
+    ["Vs"] = "V-LINE",
+    ["\22"] = "V-BLOCK",
+    ["\22s"] = "V-BLOCK",
+    ["s"] = "SELECT",
+    ["S"] = "S-LINE",
+    ["\19"] = "S-BLOCK",
+    ["i"] = "INSERT",
+    ["ic"] = "INSERT",
+    ["ix"] = "INSERT",
+    ["R"] = "REPLACE",
+    ["Rc"] = "REPLACE",
+    ["Rx"] = "REPLACE",
+    ["Rv"] = "V-REPLACE",
+    ["Rvc"] = "V-REPLACE",
+    ["Rvx"] = "V-REPLACE",
+    ["c"] = "COMMAND",
+    ["cv"] = "EX",
+    ["ce"] = "EX",
+    ["r"] = "REPLACE",
+    ["rm"] = "MORE",
+    ["r?"] = "CONFIRM",
+    ["!"] = "SHELL",
+    ["t"] = "TERMINAL",
+}
+
+local ModeColors = {
+    n = "red",
+    i = "green",
+    v = "cyan",
+    V = "cyan",
+    ["\22"] = "cyan",
+    c = "orange",
+    s = "purple",
+    S = "purple",
+    ["\19"] = "purple",
+    R = "orange",
+    r = "orange",
+    ["!"] = "red",
+    t = "red",
+}
+
+local Mode = {
+    init = function(self)
+        self.mode = vim.fn.mode(1) -- :h mode()
+    end,
+    provider = function(self)
+        local mode_name = ModeNames[self.mode] or "???"
+        return " %2(" .. mode_name .. "%)"
+    end,
+    hl = function(self)
+        local mode = self.mode:sub(1, 1) -- get only the first mode character
+        return { fg = ModeColors[mode], bold = true }
+    end,
+    update = {
+        { "ModeChanged", "VimResized" },
+        pattern = "*:*",
+        callback = vim.schedule_wrap(function()
+            vim.cmd("redrawstatus")
+        end),
+    },
+}
+
+local StatusLine = {}
+
+require("heirline").setup({
+    statusline = StatusLine,
+    opts = {
+        colors = colors,
+    },
+})
