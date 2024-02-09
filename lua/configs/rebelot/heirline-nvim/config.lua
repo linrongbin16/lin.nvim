a83a9881715d2cc794acf2aef8165bbbef3660fa
linrongbin16 <linrongbin16@outlook.com>
2024-02-09 22:00:39 +0800

    chore

--- lua/configs/rebelot/heirline-nvim/config.lua
+++ lua/configs/rebelot/heirline-nvim/config.lua
@@ -540,7 +540,7 @@ local Location = {
 
 local Progress = {
     init = function(self)
-        self.mode = vim.fn.mode(1)
+        self.mode = vim.api.nvim_get_mode().mode
     end,
     hl = function(self)
         local mode_name = GetModeName(self.mode)
