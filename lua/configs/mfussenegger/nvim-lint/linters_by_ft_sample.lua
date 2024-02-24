12560fce9a422a2a742648683b9ea60bd8d2033e
linrongbin16 <linrongbin16@outlook.com>
2024-02-24 07:57:39 -0800

    fix(nvim-lint): disable template linters (#487)

--- lua/configs/mfussenegger/nvim-lint/linters_by_ft_sample.lua
+++ lua/configs/mfussenegger/nvim-lint/linters_by_ft_sample.lua
@@ -3,13 +3,13 @@
 -- Configure linters by filetypes.
 
 local linters_by_ft = {
-    lua = { "selene", "luacheck" },
-    markdown = { "markdownlint" },
-    python = { "ruff", "mypy", "pylint" },
-    javascript = { "biome", "eslint_d", "eslint" },
-    javascriptreact = { "biome", "eslint_d", "eslint" },
-    typescript = { "biome", "eslint_d", "eslint" },
-    typescriptreact = { "biome", "eslint_d", "eslint" },
+    -- lua = { "selene", "luacheck" },
+    -- markdown = { "markdownlint" },
+    -- python = { "ruff", "mypy", "pylint" },
+    -- javascript = { "biome", "eslint_d", "eslint" },
+    -- javascriptreact = { "biome", "eslint_d", "eslint" },
+    -- typescript = { "biome", "eslint_d", "eslint" },
+    -- typescriptreact = { "biome", "eslint_d", "eslint" },
 }
 
 return linters_by_ft
