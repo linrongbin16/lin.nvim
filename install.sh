bc211741ffe46648f479e3d851ca55df6d3d3878
linrongbin16 <linrongbin16@outlook.com>
2024-02-26 09:15:51 +0800

    chore

--- install.sh
+++ install.sh
@@ -101,7 +101,7 @@ install_linux_ctags() {
 
 install_apt_nvim() {
     info "install 'nvim'(appimage) from github.com"
-    sudo apt-get -q -y install fuse
+    sudo apt-get -qq -y install fuse
     wget https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
     sudo mkdir -p /usr/local/bin
     chmod u+x nvim.appimage
@@ -111,27 +111,27 @@ install_apt_nvim() {
 install_apt_node() {
     # see: https://github.com/nodesource/distributions
     info "install nodejs from deb.nodesource.com"
-    sudo apt-get -q -y install ca-certificates gnupg
+    sudo apt-get -qq -y install ca-certificates gnupg
     sudo mkdir -p /etc/apt/keyrings
     curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
     NODE_MAJOR=20
     echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-    sudo apt-get -q update
-    sudo apt-get -q -y install nodejs
+    sudo apt-get -qq update
+    sudo apt-get -qq -y install nodejs
 }
 
 install_apt_ctags() {
-    sudo apt-get -q -y install libseccomp-dev
-    sudo apt-get -q -y install libjansson-dev
-    sudo apt-get -q -y install libyaml-dev
-    sudo apt-get -q -y install libxml2-dev
+    sudo apt-get -qq -y install libseccomp-dev
+    sudo apt-get -qq -y install libjansson-dev
+    sudo apt-get -qq -y install libyaml-dev
+    sudo apt-get -qq -y install libxml2-dev
     install_linux_ctags
 }
 
 install_apt_git() {
     sudo apt-add-repository ppa:git-core/ppa
-    sudo apt-get -q update
-    sudo apt-get -q -y install git
+    sudo apt-get -qq update
+    sudo apt-get -qq -y install git
 }
 
 install_apt() {
@@ -144,34 +144,34 @@ install_apt() {
     install_func "install_apt_nvim" "nvim"
 
     # c++ toolchain
-    install "sudo apt-get -q -y install build-essential" "gcc"
-    install "sudo apt-get -q -y install build-essential" "make"
-    install "sudo apt-get -q -y install autoconf" "autoconf"
-    install "sudo apt-get -q -y install automake" "automake"
-    install "sudo apt-get -q -y install pkg-config" "pkg-config"
-    install "sudo apt-get -q -y install cmake" "cmake"
+    install "sudo apt-get -qq -y install build-essential" "gcc"
+    install "sudo apt-get -qq -y install build-essential" "make"
+    install "sudo apt-get -qq -y install autoconf" "autoconf"
+    install "sudo apt-get -qq -y install automake" "automake"
+    install "sudo apt-get -qq -y install pkg-config" "pkg-config"
+    install "sudo apt-get -qq -y install cmake" "cmake"
 
     # download tools
     install_func "install_apt_git" "git"
-    install "sudo apt-get -q -y install curl" "curl"
-    install "sudo apt-get -q -y install wget" "wget"
+    install "sudo apt-get -qq -y install curl" "curl"
+    install "sudo apt-get -qq -y install wget" "wget"
 
     # compress tools
-    install "sudo apt-get -q -y install p7zip" "7z"
-    install "sudo apt-get -q -y install gzip" "gzip"
-    install "sudo apt-get -q -y install unzip" "unzip"
+    install "sudo apt-get -qq -y install p7zip" "7z"
+    install "sudo apt-get -qq -y install gzip" "gzip"
+    install "sudo apt-get -qq -y install unzip" "unzip"
 
     # luarocks
-    install "sudo apt-get -q -y install luajit" "luajit"
-    install "sudo apt-get -q -y install luarocks" "luarocks"
+    install "sudo apt-get -qq -y install luajit" "luajit"
+    install "sudo apt-get -qq -y install luarocks" "luarocks"
 
     # copy/paste tools
-    install "sudo apt-get -q -y install xsel" "xsel"
-    install "sudo apt-get -q -y install xclip" "xclip"
+    install "sudo apt-get -qq -y install xsel" "xsel"
+    install "sudo apt-get -qq -y install xclip" "xclip"
 
     # python3
-    install "sudo apt-get -q -y install python3 python3-dev python3-venv python3-pip python3-docutils python3-pynvim" "python3"
-    install "sudo apt-get -q -y install python3 python3-dev python3-venv python3-pip python3-docutils python3-pynvim" "pip3"
+    install "sudo apt-get -qq -y install python3 python3-dev python3-venv python3-pip python3-docutils python3-pynvim" "python3"
+    install "sudo apt-get -qq -y install python3 python3-dev python3-venv python3-pip python3-docutils python3-pynvim" "pip3"
 
     # nodejs
     install_func "install_apt_node" "node"
