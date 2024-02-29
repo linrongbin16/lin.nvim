881b99d25c43688ebe262e5e37e133990a18b8e1
linrongbin16 <linrongbin16@outlook.com>
2024-02-29 17:44:09 +0800

    chore

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -84,17 +84,6 @@ local M = {
         init = lua_init("andymass/vim-matchup"),
     },
     {
-        "inkarkat/vim-ingo-library",
-        lazy = true,
-    },
-    {
-        "inkarkat/vim-mark",
-        event = { CmdlineEnter },
-        dependencies = { "inkarkat/vim-ingo-library" },
-        init = lua_init("inkarkat/vim-mark"),
-        keys = lua_keys("inkarkat/vim-mark"),
-    },
-    {
         "markonm/traces.vim",
         event = { CmdlineEnter },
     },
