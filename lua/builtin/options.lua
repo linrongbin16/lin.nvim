ebcca71a85d0500c5beafdd766bb3c5e904ca6ba
linrongbin16 <linrongbin16@outlook.com>
2024-05-08 16:15:04 +0800

    refactor(options): use 2 spaces for indent width

--- lua/builtin/options.lua
+++ lua/builtin/options.lua
@@ -16,9 +16,9 @@ vim.opt.smartindent = true
 vim.opt.autoindent = true
 vim.opt.expandtab = true
 vim.opt.smarttab = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
 
 vim.g.editorconfig = true
 vim.opt.exrc = true
