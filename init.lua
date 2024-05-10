b95336fd69e865bf97d375b7c513313e06a5f0ee
linrongbin16 <linrongbin16@outlook.com>
2024-05-10 09:59:21 +0800

    chore

--- init.lua
+++ init.lua
@@ -4,17 +4,17 @@ local uv = vim.uv or vim.loop
 local stdpath_config = vim.fn.stdpath("config")
 
 local function vimloader(handle)
-    local vimfile = stdpath_config .. string.format("/%s.vim", handle)
-    if uv.fs_stat(vimfile) then
-        vim.fn.execute(string.format("source %s", vimfile), true)
-    end
+  local vimfile = stdpath_config .. string.format("/%s.vim", handle)
+  if uv.fs_stat(vimfile) then
+    vim.fn.execute(string.format("source %s", vimfile), true)
+  end
 end
 
 local function lualoader(handle)
-    local luafile = stdpath_config .. string.format("/lua/%s.lua", handle)
-    if uv.fs_stat(luafile) then
-        require(handle)
-    end
+  local luafile = stdpath_config .. string.format("/lua/%s.lua", handle)
+  if uv.fs_stat(luafile) then
+    require(handle)
+  end
 end
 
 -- preinit.vim and preinit.lua
@@ -24,6 +24,7 @@ lualoader("preinit")
 -- options
 require("builtin.options")
 require("builtin.lsp")
+require("builtin.diagnostic")
 
 -- plugins
 require("configs.folke.lazy-nvim.config")
---
+++ lua/builtin/diagnostic.lua
@@ -0,0 +1,69 @@
+-- ---- Diagnostic ----
+
+local constants = require("builtin.constants")
+
+local function setup()
+  local highlights_def = {
+    DiagnosticError = { "ErrorMsg", "#ff0000" },
+    DiagnosticWarn = { "WarningMsg", "#FFFF00" },
+    DiagnosticInfo = { "Normal", "#00FFFF" },
+    DiagnosticHint = { "Comment", "#808080" },
+    DiagnosticOk = { "Normal", "#008000" },
+  }
+  for name, hl in pairs(highlights_def) do
+    local old_hl = vim.api.nvim_get_hl(0, { name = name })
+    if type(old_hl) ~= "table" or vim.tbl_isempty(old_hl) then
+      local new_hl = vim.api.nvim_get_hl(0, { name = hl[1] })
+      if type(new_hl) == "table" and not vim.tbl_isempty(new_hl) then
+        vim.api.nvim_set_hl(0, name, { link = hl[1] })
+      else
+        vim.api.nvim_set_hl(0, name, { fg = hl[2] })
+      end
+    end
+  end
+
+  vim.diagnostic.config({
+    virtual_text = false,
+    severity_sort = true,
+    float = {
+      border = constants.window.border,
+      source = "always",
+      header = "",
+      prefix = "",
+    },
+    signs = {
+      text = {
+        [vim.diagnostic.severity.ERROR] = constants.diagnostic.signs.error,
+        [vim.diagnostic.severity.WARN] = constants.diagnostic.signs.warning,
+        [vim.diagnostic.severity.INFO] = constants.diagnostic.signs.info,
+        [vim.diagnostic.severity.HINT] = constants.diagnostic.signs.hint,
+      },
+    },
+  })
+
+  local signs_def = {
+    DiagnosticSignError = constants.diagnostic.signs.error,
+    DiagnosticSignWarn = constants.diagnostic.signs.warning,
+    DiagnosticSignInfo = constants.diagnostic.signs.info,
+    DiagnosticSignHint = constants.diagnostic.signs.hint,
+    DiagnosticSignOk = constants.diagnostic.signs.ok,
+  }
+  for name, text in pairs(signs_def) do
+    vim.fn.sign_define(name, {
+      texthl = name,
+      text = text,
+      numhl = "",
+    })
+  end
+end
+
+vim.api.nvim_create_augroup("builtin_diagnostic_augroup", { clear = true })
+vim.api.nvim_create_autocmd("ColorScheme", {
+  group = "builtin_diagnostic_augroup",
+  callback = vim.schedule_wrap(setup),
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  group = "builtin_diagnostic_augroup",
+  callback = vim.schedule_wrap(setup),
+})
