4185fac73d641d0922ef30bee45aa8bb87634f11
linrongbin16 <linrongbin16@outlook.com>
2024-05-15 10:55:19 +0800

    chore

--- lua/configs/akinsho/bufferline-nvim/keys.lua
+++ lua/configs/akinsho/bufferline-nvim/keys.lua
@@ -1,14 +1,6 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-  -- go to the last buffer
-  set_lazy_key(
-    "n",
-    "<leader>0",
-    "<cmd>lua require('bufferline').go_to_buffer(-1, true)<cr>",
-    { desc = "Go to the last buffer" }
-  ),
-
   -- go to next/previous buffer
   set_lazy_key("n", "]b", "<cmd>BufferLineCycleNext<cr>", { desc = "Go to next buffer" }),
   set_lazy_key("n", "[b", "<cmd>BufferLineCyclePrev<cr>", { desc = "Go to previous buffer" }),
@@ -21,6 +13,14 @@ local M = {
     "<cmd>BufferLineMovePrev<cr>",
     { desc = "Move buffer to previous" }
   ),
+
+  -- go to the last buffer
+  set_lazy_key(
+    "n",
+    "<leader>0",
+    "<cmd>lua require('bufferline').go_to(-1, true)<cr>",
+    { desc = "Go to the last buffer" }
+  ),
 }
 
 -- go to absolute buffer 1~9
@@ -30,7 +30,7 @@ for i = 1, 9 do
     set_lazy_key(
       "n",
       string.format("<leader>%d", i),
-      string.format("<cmd>lua require('bufferline').go_to_buffer(%d, true)<cr>", i),
+      string.format("<cmd>lua require('bufferline').go_to(%d, true)<cr>", i),
       { desc = string.format("Go to buffer-%d", i) }
     )
   )
