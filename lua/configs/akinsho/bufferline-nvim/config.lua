038fd419029a62ac7cd8672e5cd2bf9326a09936
linrongbin16 <linrongbin16@outlook.com>
2024-05-15 10:58:55 +0800

    chore

--- lua/configs/akinsho/bufferline-nvim/config.lua
+++ lua/configs/akinsho/bufferline-nvim/config.lua
@@ -4,9 +4,6 @@ local str = require("commons.str")
 
 require("bufferline").setup({
   options = {
-    -- numbers = function(opts)
-    --     return string.format("%s.%s", opts.ordinal, opts.lower(opts.id))
-    -- end,
     numbers = "ordinal",
     close_command = "Bdelete! %d", -- Bdelete: https://github.com/moll/vim-bbye
     right_mouse_command = "Bdelete! %d",
