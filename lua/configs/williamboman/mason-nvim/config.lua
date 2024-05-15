d4673b662b0ae24d796ac14692cd1be4b2c4e121
linrongbin16 <linrongbin16@outlook.com>
2024-05-15 15:16:38 +0800

    chore

--- lua/configs/williamboman/mason-nvim/config.lua
+++ lua/configs/williamboman/mason-nvim/config.lua
@@ -1,7 +1,8 @@
 local constants = require("builtin.constants")
+local path = require("mason-core.path")
 
 require("mason").setup({
-  install_root_dir = vim.fn.stdpath("config") .. "/mason",
+  install_root_dir = path.concat({ vim.fn.stdpath("config"), "mason" }),
   ui = {
     border = constants.window.border,
     width = constants.window.layout.middle.scale,
