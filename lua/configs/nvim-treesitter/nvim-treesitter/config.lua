d548e8e0ab51b2c8bda940b95f2106e4c80fffb6
linrongbin16 <linrongbin16@outlook.com>
2024-05-15 10:36:03 +0800

    chore

--- lua/configs/nvim-treesitter/nvim-treesitter/config.lua
+++ lua/configs/nvim-treesitter/nvim-treesitter/config.lua
@@ -11,7 +11,7 @@ if user_ensure_installed_ok then
   if type(user_ensure_installed) == "table" then
     ensure_installed = user_ensure_installed
   else
-    message.warn(string.format("Error loading '%s' lua module!", user_ensure_installed_module))
+    message.err(string.format("Error loading '%s' lua module!", user_ensure_installed_module))
   end
 end
 
