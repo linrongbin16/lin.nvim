9247d774e1cc6378a1d67e121e58e9e25a6ed3f8
linrongbin16 <linrongbin16@outlook.com>
2024-05-16 09:42:30 +0800

    chore

--- lua/configs/romgrk/barbar-nvim/config.lua
+++ lua/configs/romgrk/barbar-nvim/config.lua
@@ -14,3 +14,13 @@ require("barbar").setup({
   maximum_length = layout.editor.width(0.334, 60, nil),
   no_name_title = "[No Name]",
 })
+
+local function setup_color()
+  vim.api.nvim_set_hl(0, "BufferCurrentIndex", { link = "BufferCurrent" })
+end
+
+local barbar_augroup = vim.api.nvim_create_augroup("barbar_augroup", { clear = true })
+vim.api.nvim_create_autocmd({ "ColorScheme", "VimEnter" }, {
+  group = barbar_augroup,
+  callback = setup_color,
+})
