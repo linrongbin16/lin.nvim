4d63d63417a8b35d2e957584c1bc0ae2371da819
linrongbin16 <linrongbin16@outlook.com>
2024-05-16 09:28:11 +0800

    chore

--- lua/configs/romgrk/barbar-nvim/config.lua
+++ lua/configs/romgrk/barbar-nvim/config.lua
@@ -25,8 +25,9 @@ local function setup_current_color()
     "bg",
     black
   )
-  local fg = color_hl.get_color_with_fallback({ "TabLineSel", "Normal" }, "fg", white)
+  local fg = color_hl.get_color_with_fallback({ "Normal", "StatusLine", "TabLineSel" }, "fg", white)
   vim.api.nvim_set_hl(0, "BufferCurrent", { fg = fg, bg = bg })
+  vim.api.nvim_set_hl(0, "BufferIndex", { fg = fg, bg = bg })
 end
 
 local barbar_augroup = vim.api.nvim_create_augroup("barbar_augroup", { clear = true })
