a7215af87ba6470c277025d37da72a88c75a4b64
linrongbin16 <linrongbin16@outlook.com>
2024-05-16 09:34:35 +0800

    chore

--- lua/configs/romgrk/barbar-nvim/config.lua
+++ lua/configs/romgrk/barbar-nvim/config.lua
@@ -1,5 +1,4 @@
 local layout = require("builtin.utils.layout")
-local color_hl = require("commons.color.hl")
 
 require("barbar").setup({
   animation = false,
@@ -14,28 +13,4 @@ require("barbar").setup({
   },
   maximum_length = layout.editor.width(0.334, 60, nil),
   no_name_title = "[No Name]",
-})
-
-local white = "#ffffff"
-local black = "#000000"
-
-local function setup_current_color()
-  local bg = color_hl.get_color_with_fallback(
-    { "StatusLine", "PmenuSel", "PmenuThumb", "TabLineSel" },
-    "bg",
-    black
-  )
-  local fg = color_hl.get_color_with_fallback({ "Normal", "StatusLine", "TabLineSel" }, "fg", white)
-  vim.api.nvim_set_hl(0, "BufferCurrent", { fg = fg, bg = bg })
-  vim.api.nvim_set_hl(0, "BufferIndex", { fg = fg })
-end
-
-local barbar_augroup = vim.api.nvim_create_augroup("barbar_augroup", { clear = true })
-vim.api.nvim_create_autocmd("ColorScheme", {
-  group = barbar_augroup,
-  callback = setup_current_color,
-})
-vim.api.nvim_create_autocmd("VimEnter", {
-  group = barbar_augroup,
-  callback = setup_current_color,
 })
