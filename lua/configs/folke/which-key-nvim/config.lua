7be19a3d13d768d5776ae4790eb840009433a952
linrongbin16 <linrongbin16@outlook.com>
2024-07-16 11:03:23 +0800

    refactor(which-key.nvim): set 100ms for plugin keys

--- lua/configs/folke/which-key-nvim/config.lua
+++ lua/configs/folke/which-key-nvim/config.lua
@@ -1,6 +1,9 @@
 local constants = require("builtin.constants")
 
 require("which-key").setup({
+  delay = function(ctx)
+    return ctx.plugin and 100 or 200
+  end,
   win = {
     border = constants.window.border,
     wo = {
