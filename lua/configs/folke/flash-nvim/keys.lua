6925452aaaaa9f7b63d187127f9fc9bf2724f022
linrongbin16 <linrongbin16@outlook.com>
2024-08-22 09:51:51 +0800

    chore

--- lua/configs/folke/flash-nvim/keys.lua
+++ lua/configs/folke/flash-nvim/keys.lua
@@ -3,10 +3,10 @@ local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 local M = {
   set_lazy_key({ "n", "x", "o" }, "s", function()
     require("flash").jump()
-  end, { desc = "Jump forward" }),
+  end, { desc = "Jump" }),
   set_lazy_key({ "o" }, "r", function()
     require("flash").remote()
-  end, { desc = "Remote flash" }),
+  end, { desc = "Jump in operator-pending" }),
 }
 
 return M
