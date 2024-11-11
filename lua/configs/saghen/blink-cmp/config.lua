a30439167e2e2b947873152d8efbfb9299cd831e
linrongbin16 <linrongbin16@outlook.com>
2024-11-11 10:33:57 +0800

    feat(blink.cmp): migrate "nvim-cmp" to "blink.cmp" (#612)

---
+++ lua/configs/saghen/blink-cmp/config.lua
@@ -0,0 +1,23 @@
+require("blink.cmp").setup({
+  keymap = {
+    ["<CR>"] = { "accept", "fallback" },
+    ["<Tab>"] = { "accept", "fallback" },
+    ["<S-Tab>"] = { "fallback" },
+
+    ["<Up>"] = { "select_prev", "fallback" },
+    ["<Down>"] = { "select_next", "fallback" },
+    ["<C-p>"] = { "select_prev", "fallback" },
+    ["<C-n>"] = { "select_next", "fallback" },
+
+    ["<C-f>"] = { "snippet_forward", "fallback" },
+    ["<C-b>"] = { "snippet_backward", "fallback" },
+
+    ["<C-u>"] = { "scroll_documentation_up", "fallback" },
+    ["<C-d>"] = { "scroll_documentation_down", "fallback" },
+  },
+  trigger = {
+    signature_help = {
+      enabled = true,
+    },
+  },
+})
