4d9b0a61dc892ebdda27fb40e5cad5f5e83b1e91
linrongbin16 <linrongbin16@outlook.com>
2024-11-20 16:43:43 +0800

    fix(dropbar.nvim): disable lsp/treesitter sources in dropbar.nvim since it leads to neovim crash

--- lua/configs/Bekaboo/dropbar-nvim/config.lua
+++ lua/configs/Bekaboo/dropbar-nvim/config.lua
@@ -1,5 +1,10 @@
 require("dropbar").setup({
   bar = {
     update_debounce = 100,
+    sources = function(buf, _)
+      local sources = require("dropbar.sources")
+      -- local utils = require("dropbar.utils")
+      return { sources.path }
+    end,
   },
 })
