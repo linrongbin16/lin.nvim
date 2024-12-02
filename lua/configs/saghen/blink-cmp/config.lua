acb5cbf33875e586e719b219487c8b265dfbe269
linrongbin16 <linrongbin16@outlook.com>
2024-12-02 10:02:04 +0800

    feat(LuaSnip.nvim): use "LuaSnip.nvim" for snippet

--- lua/configs/saghen/blink-cmp/config.lua
+++ lua/configs/saghen/blink-cmp/config.lua
@@ -15,6 +15,20 @@ require("blink.cmp").setup({
     ["<C-u>"] = { "scroll_documentation_up", "fallback" },
     ["<C-d>"] = { "scroll_documentation_down", "fallback" },
   },
+  snippets = {
+    expand = function(snippet)
+      require("luasnip").lsp_extend(snippet)
+    end,
+    active = function(filter)
+      if filter and filter.direction then
+        return require("luasnip").jumpable(filter.direction)
+      end
+      return require("luasnip").in_snippet()
+    end,
+    jump = function(direction)
+      require("luasnip").jump(direction)
+    end,
+  },
   trigger = {
     accept = { auto_brackets = { enabled = true } },
     signature_help = {
