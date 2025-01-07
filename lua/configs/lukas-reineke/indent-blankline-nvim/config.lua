c5add53ef26036a0c78d6587e005db4cec498785
linrongbin16 <linrongbin16@outlook.com>
2025-01-07 13:15:30 +0800

    perf(bigfile): disable indentline-blank for big file (#668)

--- lua/configs/lukas-reineke/indent-blankline-nvim/config.lua
+++ lua/configs/lukas-reineke/indent-blankline-nvim/config.lua
@@ -1,3 +1,11 @@
+local constants = require("builtin.constants")
+
 require("ibl").setup({
   scope = { enabled = false },
 })
+
+local hooks = require("ibl.hooks")
+hooks.register(hooks.type.ACTIVE, function(bufnr)
+  local f = vim.api.nvim_buf_get_name(bufnr)
+  return vim.fn.getfsize(f) <= constants.perf.maxfilesize
+end)
