1f49174804886afd167cfce8848b07e26a816bee
linrongbin16 <linrongbin16@outlook.com>
2025-01-07 17:51:15 +0800

    chore

--- lua/configs/lukas-reineke/indent-blankline-nvim/config.lua
+++ lua/configs/lukas-reineke/indent-blankline-nvim/config.lua
@@ -1,11 +1,9 @@
-local constants = require("builtin.constants")
-
 require("ibl").setup({
   scope = { enabled = false },
 })
 
 local hooks = require("ibl.hooks")
 hooks.register(hooks.type.ACTIVE, function(bufnr)
-  local f = vim.api.nvim_buf_get_name(bufnr)
-  return vim.fn.getfsize(f) <= constants.perf.maxfilesize
+  local performance = require("builtin.performance")
+  return not performance.file_too_big(bufnr)
 end)
