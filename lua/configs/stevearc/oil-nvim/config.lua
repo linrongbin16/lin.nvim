037451aaf017342d06d6174652ece976d68d5921
linrongbin16 <linrongbin16@outlook.com>
2025-01-09 06:07:22 +0800

    chore

--- lua/configs/stevearc/oil-nvim/config.lua
+++ lua/configs/stevearc/oil-nvim/config.lua
@@ -9,24 +9,6 @@ require("oil").setup({
   },
   delete_to_trash = vim.fn.executable("trash") > 0,
   trash_command = "trash",
-  keymaps = {
-    -- open in new tab/split/vsplit
-    ["<C-w>v"] = "actions.select_vsplit",
-    ["<C-s>"] = false,
-    ["<C-w>s"] = "actions.select_split",
-    ["<C-h>"] = false,
-    ["<C-w>t"] = "actions.select_tab",
-    ["<C-t>"] = false,
-
-    -- preview, refresh
-    ["<C-l>"] = "actions.preview",
-    ["<C-p>"] = false,
-    ["<C-r>"] = "actions.refresh",
-
-    -- go to upper folder
-    ["<BS>"] = "actions.parent",
-    ["-"] = false,
-  },
   view_options = {
     show_hidden = true,
   },
