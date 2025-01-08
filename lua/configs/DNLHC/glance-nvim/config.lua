9154802b7b6317f09a92e8af01d72bd90d42dd15
linrongbin16 <linrongbin16@outlook.com>
2025-01-09 06:03:45 +0800

    chore

--- lua/configs/DNLHC/glance-nvim/config.lua
+++ lua/configs/DNLHC/glance-nvim/config.lua
@@ -1,29 +1,6 @@
 local glance = require("glance")
-local actions = glance.actions
 local layout = require("builtin.utils.layout")
 
 glance.setup({
-  height = layout.editor.height(0.99, 7, 18),
-  mappings = {
-    list = {
-      -- open
-      ["<C-w>s"] = actions.jump_split,
-      ["s"] = false,
-      ["<C-w>v"] = actions.jump_vsplit,
-      ["v"] = false,
-      ["<C-w>t"] = actions.jump_tab,
-      ["t"] = false,
-
-      -- disable go to preview window
-      ["<Leader>l"] = false,
-    },
-    preview = {
-      -- disable navigation
-      ["<Tab>"] = false,
-      ["<S-Tab>"] = false,
-
-      -- disable go to list window
-      ["<Leader>l"] = false,
-    },
-  },
+  height = layout.editor.height(0.99, 3, 18),
 })
