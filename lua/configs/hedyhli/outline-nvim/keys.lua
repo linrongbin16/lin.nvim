0aefc08428245cd40eddaf7caa16f8260dc859f9
linrongbin16 <linrongbin16@outlook.com>
2025-01-08 16:03:17 +0800

    feat(outline.nvim): add "<leader>or" for refreshing outline

--- lua/configs/hedyhli/outline-nvim/keys.lua
+++ lua/configs/hedyhli/outline-nvim/keys.lua
@@ -2,6 +2,7 @@ local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
   set_lazy_key("n", "<leader>ol", "<cmd>Outline<cr>", { desc = "Toggle outline" }),
+  set_lazy_key("n", "<leader>or", "<cmd>OutlineRefresh<cr>", { desc = "Refresh outline" }),
 }
 
 return M
