f9d6292fa1d526f5ed675caa2f97af82aa81e9de
linrongbin16 <linrongbin16@outlook.com>
2025-01-08 11:37:21 +0800

    chore(blink.cmp): upgrade blink.cmp configs

--- lua/configs/saghen/blink-cmp/config.lua
+++ lua/configs/saghen/blink-cmp/config.lua
@@ -56,21 +56,5 @@ require("blink.cmp").setup({
   signature = {
     enabled = true,
   },
-  sources = {
-    default = { "lsp", "path", "luasnip", "buffer" },
-  },
-  snippets = {
-    expand = function(snippet)
-      require("luasnip").lsp_expand(snippet)
-    end,
-    active = function(filter)
-      if filter and filter.direction then
-        return require("luasnip").jumpable(filter.direction)
-      end
-      return require("luasnip").in_snippet()
-    end,
-    jump = function(direction)
-      require("luasnip").jump(direction)
-    end,
-  },
+  snippets = { preset = "luasnip" },
 })
