7b7c41430523f8793bd98c5f269288ff7b467b2c
linrongbin16 <linrongbin16@outlook.com>
2025-02-13 09:38:46 +0800

    chore

--- lua/configs/folke/snacks-nvim/config.lua
+++ lua/configs/folke/snacks-nvim/config.lua
@@ -1,4 +1,9 @@
+local constants = require("builtin.constants")
+
 require("snacks").setup({
   indent = { enabled = true, animate = { enabled = false }, scope = { enabled = false } },
-  input = { enabled = true, win = { relative = "cursor", row = 1, col = 0 } },
+  input = {
+    enabled = true,
+    win = { border = constants.window.border, relative = "cursor", row = 1, col = 0 },
+  },
 })
