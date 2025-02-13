88fa18de49f5c48e9968142659bb9f533dcc9457
linrongbin16 <linrongbin16@outlook.com>
2025-02-13 09:58:46 +0800

    chore

--- lua/configs/folke/snacks-nvim/config.lua
+++ lua/configs/folke/snacks-nvim/config.lua
@@ -5,5 +5,8 @@ require("snacks").setup({
   input = {
     enabled = true,
     win = { border = constants.window.border, relative = "cursor", row = 1, col = 0 },
+    keys = {
+      i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i", expr = true },
+    },
   },
 })
