9b2612f1e5359cf2fd915e62e13346a5823ae9b6
linrongbin16 <linrongbin16@outlook.com>
2025-02-13 10:12:28 +0800

    chore

--- lua/configs/folke/snacks-nvim/config.lua
+++ lua/configs/folke/snacks-nvim/config.lua
@@ -1,12 +1,19 @@
 local constants = require("builtin.constants")
 
 require("snacks").setup({
+  styles = {
+    input = {
+      border = constants.window.border,
+      relative = "cursor",
+      row = 1,
+      col = 0,
+      keys = {
+        i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i", expr = true },
+      },
+    },
+  },
   indent = { enabled = true, animate = { enabled = false }, scope = { enabled = false } },
   input = {
     enabled = true,
-    win = { border = constants.window.border, relative = "cursor", row = 1, col = 0 },
-    keys = {
-      i_esc = { "<esc>", { "cmp_close", "cancel" }, mode = "i", expr = true },
-    },
   },
 })
