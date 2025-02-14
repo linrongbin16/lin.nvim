142b53dcb7bf0f78427595221fce0906037257f4
linrongbin16 <linrongbin16@outlook.com>
2025-02-14 09:16:32 +0800

    refactor(ui): adjust vim.ui width

--- lua/builtin/constants.lua
+++ lua/builtin/constants.lua
@@ -15,32 +15,12 @@ M.diagnostic = {
 M.window = {
   border = "rounded", -- single,double,rounded,solid,shadow
   layout = {
-    middle = {
-      scale = 0.85,
-    },
-    large = {
-      scale = 0.9,
-    },
-    sidebar = {
-      scale = 0.2,
-      min = 20,
-      max = 60,
-    },
-    input = {
-      scale = 0.3,
-      min = 20,
-      max = 60,
-    },
-    select = {
-      scale = 0.4,
-      min = 30,
-      max = 80,
-    },
-    cmdline = {
-      scale = 0.4,
-      min = 30,
-      max = 80,
-    },
+    middle = { scale = 0.85 },
+    large = { scale = 0.9 },
+    sidebar = { scale = 0.2, min = 20, max = 60 },
+    input = { scale = 0.35, min = 30, max = 80 },
+    select = { scale = 0.5, min = 40, max = 100 },
+    cmdline = { scale = 0.5, min = 40, max = 100 },
   },
   blend = 15,
 }
