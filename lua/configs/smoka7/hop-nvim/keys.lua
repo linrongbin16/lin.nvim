6bbbd2034cca9bfa5e3c2b0acdb32b0a3a255b8f
linrongbin16 <linrongbin16@outlook.com>
2025-02-21 16:44:35 +0800

    chore

--- lua/configs/smoka7/hop-nvim/keys.lua
+++ lua/configs/smoka7/hop-nvim/keys.lua
@@ -1,30 +1,6 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-  set_lazy_key(
-    { "n", "x" },
-    "<leader>f",
-    "<cmd>HopChar1AC<cr>",
-    { desc = "Jump forward by {char}" }
-  ),
-  set_lazy_key(
-    { "n", "x" },
-    "<leader>F",
-    "<cmd>HopChar1BC<cr>",
-    { desc = "Jump backward by {char}" }
-  ),
-  set_lazy_key(
-    { "n", "x" },
-    "<leader>s",
-    "<cmd>HopChar2AC<cr>",
-    { desc = "Jump forward by {char}{char}" }
-  ),
-  set_lazy_key(
-    { "n", "x" },
-    "<leader>S",
-    "<cmd>HopChar2BC<cr>",
-    { desc = "Jump backward by {char}{char}" }
-  ),
   set_lazy_key({ "n", "x" }, "<leader>j", "<cmd>HopLineAC<cr>", { desc = "Jump to below lines" }),
   set_lazy_key({ "n", "x" }, "<leader>k", "<cmd>HopLineBC<cr>", { desc = "Jump to above lines" }),
 }
