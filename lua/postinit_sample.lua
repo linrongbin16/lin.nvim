79ad3135ef53347d5c02acd15270cd24b9a6dff9
linrongbin16 <linrongbin16@outlook.com>
2025-03-26 12:02:08 +0800

    chore(hooks): add neovide configs

--- lua/postinit_sample.lua
+++ lua/postinit_sample.lua
@@ -7,3 +7,10 @@ end
 if vim.fn.has("mac") > 0 and vim.fn.exists("$VIMRUNTIME/macmap.vim") > 0 then
   vim.cmd([[source $VIMRUNTIME/macmap.vim]])
 end
+
+-- neovide configs
+if vim.g.neovide then
+  vim.o.guifont = "Monaco Nerd Font Mono:h12"
+  vim.g.neovide_scroll_animation_length = 0.0
+  vim.g.neovide_position_animation_length = 0.0
+end
