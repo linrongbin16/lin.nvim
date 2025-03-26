171dd7e0d7d49125879c05a6d4621b63c874a9e9
linrongbin16 <linrongbin16@outlook.com>
2025-03-26 13:01:17 +0800

    chore(hooks): add neovide configs

--- lua/postinit_sample.lua
+++ lua/postinit_sample.lua
@@ -11,7 +11,12 @@ end
 -- neovide configs
 if vim.g.neovide then
   vim.o.guifont = "Monaco Nerd Font Mono:h12"
-  vim.g.neovide_scroll_animation_length = 0.0
-  vim.g.neovide_position_animation_length = 0.0
   vim.g.neovide_hide_mouse_when_typing = true
+  vim.g.neovide_position_animation_length = 0
+  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_animate_in_insert_mode = false
+  vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_scroll_animation_far_lines = 0
+  vim.g.neovide_scroll_animation_length = 0.00
 end
