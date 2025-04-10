24871b0d1418d2216ddf163390d8c3dad70c8a00
linrongbin16 <linrongbin16@outlook.com>
2025-04-10 17:09:41 +0800

    chore(neovide): disable neovide animations

--- lua/postinit_sample.lua
+++ lua/postinit_sample.lua
@@ -13,10 +13,12 @@ if vim.g.neovide then
   vim.o.guifont = "Monaco Nerd Font Mono:h12"
   vim.g.neovide_hide_mouse_when_typing = true
   vim.g.neovide_position_animation_length = 0
-  vim.g.neovide_cursor_animation_length = 0.00
+  vim.g.neovide_cursor_animation_length = 0
   vim.g.neovide_cursor_trail_size = 0
+  vim.g.neovide_cursor_short_animation_length = 0
   vim.g.neovide_cursor_animate_in_insert_mode = false
   vim.g.neovide_cursor_animate_command_line = false
+  vim.g.neovide_cursor_smooth_blink = false
   vim.g.neovide_scroll_animation_far_lines = 0
-  vim.g.neovide_scroll_animation_length = 0.00
+  vim.g.neovide_scroll_animation_length = 0
 end
