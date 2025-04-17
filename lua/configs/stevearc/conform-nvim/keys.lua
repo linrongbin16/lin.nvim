e49fd5ba39b1fa0a1769b11e0b2acd63c70b41ba
linrongbin16 <linrongbin16@outlook.com>
2025-04-17 09:35:19 +0800

    chore

--- lua/configs/stevearc/conform-nvim/keys.lua
+++ lua/configs/stevearc/conform-nvim/keys.lua
@@ -1,8 +1,26 @@
+local message = require("builtin.utils.message")
 local keymap = require("builtin.utils.keymap")
 
 local M = {
-  keymap.set_lazy_key({ "n", "x" }, "<Leader>cf", function()
-    require("conform").format({ async = true, timeout_ms = 2000, lsp_fallback = "fallback" })
+  keymap.set_lazy_key({ "n" }, "<Leader>cf", function()
+    require("conform").format(
+      { async = true, timeout_ms = 2000, lsp_fallback = "fallback" },
+      function(err, did_edit)
+        if err then
+          message.warn(err)
+        end
+      end
+    )
+  end, { desc = "Code format" }),
+  keymap.set_lazy_key({ "x" }, "<Leader>cf", function()
+    require("conform").format(
+      { async = true, timeout_ms = 2000, lsp_fallback = "fallback" },
+      function(err, did_edit)
+        if err then
+          message.warn(err)
+        end
+      end
+    )
   end, { desc = "Code format" }),
 }
 
