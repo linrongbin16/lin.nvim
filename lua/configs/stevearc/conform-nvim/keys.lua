d789a77751e6e1aca3c5a2fa0ccbf5e7dfe149a4
linrongbin16 <linrongbin16@outlook.com>
2025-04-17 09:31:20 +0800

    fix(conform.nvim): fix conform.format parameters with 'async=true'

--- lua/configs/stevearc/conform-nvim/keys.lua
+++ lua/configs/stevearc/conform-nvim/keys.lua
@@ -1,12 +1,9 @@
 local keymap = require("builtin.utils.keymap")
 
 local M = {
-  keymap.set_lazy_key(
-    { "n", "x" },
-    "<Leader>cf",
-    "<cmd>lua require('conform').format({ timeout_ms = 2000, lsp_fallback = true })<cr>",
-    { desc = "Code format" }
-  ),
+  keymap.set_lazy_key({ "n", "x" }, "<Leader>cf", function()
+    require("conform").format({ async = true, timeout_ms = 2000, lsp_fallback = "fallback" })
+  end, { desc = "Code format" }),
 }
 
 return M
