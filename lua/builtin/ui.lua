73d3452913b225f8e2d399b840dc79069e415da9
linrongbin16 <linrongbin16@outlook.com>
2025-04-30 21:59:55 +0800

    fix: allow neovim v0.10 compatibility (#733)

--- lua/builtin/ui.lua
+++ lua/builtin/ui.lua
@@ -3,7 +3,9 @@
 local constants = require("builtin.constants")
 
 -- window border
-vim.g.winborder = constants.window.border
+if vim.fn.has("nvim-0.11") > 0 then
+  vim.g.winborder = constants.window.border
+end
 
 -- transparent
 vim.o.winblend = constants.window.blend
