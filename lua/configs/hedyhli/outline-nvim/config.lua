72bd1b05436c1ab4e7b541016c031f34663c5608
linrongbin16 <linrongbin16@outlook.com>
2025-05-17 14:56:56 +0800

    feat(outline.nvim): add two key mappings to adjust outline window width (#738)

--- lua/configs/hedyhli/outline-nvim/config.lua
+++ lua/configs/hedyhli/outline-nvim/config.lua
@@ -27,3 +27,15 @@ require("outline").setup({
     },
   },
 })
+
+local outline_augroup = vim.api.nvim_create_augroup("outline_augroup", { clear = true })
+vim.api.nvim_create_autocmd("FileType", {
+  group = outline_augroup,
+  pattern = "Outline",
+  callback = function()
+    local set_key = require("builtin.utils.keymap").set_key
+    local opts = { buffer = true }
+    set_key("n", "<leader>.", "<cmd>vertical resize -10<cr>", opts)
+    set_key("n", "<leader>,", "<cmd>vertical resize +10<cr>", opts)
+  end,
+})
