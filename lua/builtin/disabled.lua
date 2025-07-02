1239d70938013c26517d15597030a6e38b4d139d
linrongbin16 <linrongbin16@outlook.com>
2025-07-02 18:02:49 +0800

    chore

--- lua/builtin/disabled.lua
+++ lua/builtin/disabled.lua
@@ -6,3 +6,10 @@ vim.g.loaded_netrwPlugin = 1
 -- Replace with 'vim-matchup'
 vim.g.loaded_matchit = 1
 vim.g.loaded_matchparen = 1
+
+-- Not used
+vim.g.loaded_gzip = 1
+vim.g.loaded_tarPlugin = 1
+vim.g.loaded_zipPlugin = 1
+vim.g.loaded_tutor = 1
+vim.g.loaded_tohtml = 1
