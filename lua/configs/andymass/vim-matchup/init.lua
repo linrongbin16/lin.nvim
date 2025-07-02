c129e614bbddc980505e3e1f7d084ae442c9b259
linrongbin16 <linrongbin16@outlook.com>
2025-07-02 17:55:24 +0800

    perf(matchup): skip 'matchparen' plugin

--- lua/configs/andymass/vim-matchup/init.lua
+++ lua/configs/andymass/vim-matchup/init.lua
@@ -1,5 +1,6 @@
 -- Disable matchit
 vim.g.loaded_matchit = 1
+vim.g.loaded_matchparen = 1
 vim.g.matchup_matchparen_offscreen = { method = "popup" }
 -- vim.g.matchup_matchparen_offscreen = { method = "status_manual" }
 
