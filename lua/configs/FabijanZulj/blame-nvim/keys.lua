a76d6f483a20ae79de767fc38003a7919adaceed
linrongbin16 <linrongbin16@outlook.com>
2025-07-21 17:24:33 +0800

    chore

--- lua/configs/FabijanZulj/blame-nvim/keys.lua
+++ lua/configs/FabijanZulj/blame-nvim/keys.lua
@@ -2,7 +2,7 @@ local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
   -- toggle git blame
-  set_lazy_key("n", "<leader>gb", "<cmd>BlameToggle virtual<cr>", { desc = "Toggle git blame" }),
+  set_lazy_key("n", "<leader>gb", "<cmd>BlameToggle<cr>", { desc = "Toggle git blame" }),
 }
 
 return M
