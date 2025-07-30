be1c0b1dcd0da4e9d69b273b54f41fdac7db5ed4
linrongbin16 <linrongbin16@outlook.com>
2025-07-30 10:57:51 +0800

    chore

--- lua/plugins/users_sample.lua
+++ lua/plugins/users_sample.lua
@@ -21,14 +21,7 @@
 --   {
 --     "m4xshen/hardtime.nvim",
 --     dependencies = { "MunifTanjim/nui.nvim" },
---     opts = {
---       disabled_keys = {
---         ["<Up>"] = {},
---         ["<Down>"] = {},
---         ["<Left>"] = {},
---         ["<Right>"] = {},
---       },
---     }
+--     config = lua_config("m4xshen/hardtime.nvim"),
 --   },
 --   -- No-highlight
 --   {
