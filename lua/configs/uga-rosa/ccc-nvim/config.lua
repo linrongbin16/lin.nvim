0a5391c672e0c471c1f0f2529308031b2d66a9b0
linrongbin16 <linrongbin16@outlook.com>
2025-07-30 11:00:21 +0800

    chore

--- lua/configs/uga-rosa/ccc-nvim/config.lua
+++
@@ -1,7 +0,0 @@
-local ccc_augroup = vim.api.nvim_create_augroup("ccc_augroup", { clear = true })
-vim.api.nvim_create_autocmd("BufReadPost", {
-  group = ccc_augroup,
-  callback = function()
-    vim.cmd("CccHighlighterEnable")
-  end,
-})
