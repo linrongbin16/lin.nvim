74224954e67f614c626d3388694baea4406da642
linrongbin16 <linrongbin16@outlook.com>
2025-07-30 11:03:33 +0800

    chore

--- lua/configs/Pocco81/auto-save-nvim/config.lua
+++
@@ -1,8 +0,0 @@
-require("auto-save").setup({
-  execution_message = {
-    message = function()
-      return ("[auto-save.nvim] saved at " .. vim.fn.strftime("%H:%M:%S"))
-    end,
-  },
-  debounce_delay = 1000,
-})
