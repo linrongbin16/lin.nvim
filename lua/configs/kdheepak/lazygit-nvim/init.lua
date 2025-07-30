3649bfebcd972cdaef4acd0bac358a7319074f19
linrongbin16 <linrongbin16@outlook.com>
2025-07-30 10:48:09 +0800

    chore

--- lua/configs/kdheepak/lazygit-nvim/init.lua
+++ lua/configs/kdheepak/lazygit-nvim/init.lua
@@ -1,5 +1,5 @@
 local constants = require("builtin.constants")
 
-vim.lazygit_floating_window_border_chars = constants.window.border_chars
+vim.g.lazygit_floating_window_border_chars = constants.window.border_chars
 vim.g.lazygit_floating_window_winblend = constants.window.blend
 vim.g.lazygit_floating_window_scaling_factor = constants.layout.window.scale
