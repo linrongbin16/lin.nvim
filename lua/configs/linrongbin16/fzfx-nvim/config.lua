fd82f088eabed9564aa4e7e556e989ff838d5c1e
linrongbin16 <linrongbin16@outlook.com>
2025-07-31 14:29:40 +0800

    chore

--- lua/configs/linrongbin16/fzfx-nvim/config.lua
+++ lua/configs/linrongbin16/fzfx-nvim/config.lua
@@ -4,7 +4,7 @@ local layout = require("builtin.utils.layout")
 require("fzfx").setup({
   popup = {
     win_opts = {
-      height = layout.editor.height(constants.layout.window.scale, 1) + 2,
+      height = math.min(layout.editor.height(constants.layout.window.scale, 1) + 2, vim.o.lines),
       width = layout.editor.width(constants.layout.window.scale, 1),
     },
   },
