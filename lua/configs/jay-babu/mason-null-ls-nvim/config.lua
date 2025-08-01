9ae7df84d3d521715dd49e210ac6d5fca680c92e
linrongbin16 <linrongbin16@outlook.com>
2025-08-01 09:55:57 +0800

    refactor(lsp): refactor useless lsp management options

--- lua/configs/jay-babu/mason-null-ls-nvim/config.lua
+++ lua/configs/jay-babu/mason-null-ls-nvim/config.lua
@@ -1,18 +1,1 @@
-local message = require("builtin.utils.message")
-
-local setup_handlers = {}
-
-local user_setup_handlers_module = "configs.jay-babu.mason-null-ls-nvim.setup_handlers"
-local user_setup_handlers_ok, user_setup_handlers = pcall(require, user_setup_handlers_module)
-
-if user_setup_handlers_ok then
-  if type(user_setup_handlers) == "table" then
-    setup_handlers = user_setup_handlers
-  else
-    message.err(string.format("Error loading '%s' lua module!", user_setup_handlers_module))
-  end
-end
-
-require("mason-null-ls").setup({
-  handlers = setup_handlers,
-})
+require("mason-null-ls").setup()
