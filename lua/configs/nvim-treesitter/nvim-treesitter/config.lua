12dd0b01fe2e3a64d8bd07f350104d490fe56364
linrongbin16 <linrongbin16@outlook.com>
2025-08-25 12:54:38 +0800

    feat(nvim-treesitter): upgrade configs for nvim-treesitter

--- lua/configs/nvim-treesitter/nvim-treesitter/config.lua
+++ lua/configs/nvim-treesitter/nvim-treesitter/config.lua
@@ -1,14 +1,28 @@
-local bigfile = require("builtin.utils.bigfile")
+local str = require("commons.str")
+
+local stdpath_config = vim.fn.stdpath("config")
 
 ---@diagnostic disable-next-line: missing-fields
-require("nvim-treesitter.configs").setup({
-  auto_install = true,
-  highlight = {
-    enable = true,
-    -- disable for super large file
-    disable = function(lang, buf)
-      return bigfile.is_too_big(buf)
-    end,
-    additional_vim_regex_highlighting = false,
-  },
+require("nvim-treesitter").setup({
+  install_dir = stdpath_config .. "/treesitter",
 })
+
+-- Highlight per filetype
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "<filetype>" },
+  callback = function(event)
+    if event ~= nil and type(event.buf) == "number" and vim.api.nvim_buf_is_valid(event.buf) then
+      local ft = vim.api.nvim_get_option_value("filetype", { buf = event.buf })
+      if str.not_empty(ft) then
+        require("nvim-treesitter").install({ event.buf })
+      end
+    end
+    vim.treesitter.start()
+  end,
+})
+
+-- Indentation
+vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+-- Fold
+vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
