138da09816e0d03438af39a42d121270e8e9f55b
linrongbin16 <linrongbin16@outlook.com>
2025-08-26 12:27:06 +0800

    c

--- lua/configs/nvim-treesitter/nvim-treesitter/config.lua
+++ lua/configs/nvim-treesitter/nvim-treesitter/config.lua
@@ -4,15 +4,15 @@ require("nvim-treesitter").setup({
   install_dir = stdpath_config .. "/treesitter",
 })
 
-local nvim_treesitter_augroup =
-  vim.api.nvim_create_augroup("nvim_treesitter_augroup", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = nvim_treesitter_augroup,
-  pattern = { "<filetype>" },
-  callback = function()
-    vim.treesitter.start()
-  end,
-})
-
-vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+-- local nvim_treesitter_augroup =
+--   vim.api.nvim_create_augroup("nvim_treesitter_augroup", { clear = true })
+-- vim.api.nvim_create_autocmd("FileType", {
+--   group = nvim_treesitter_augroup,
+--   pattern = { "<filetype>" },
+--   callback = function()
+--     vim.treesitter.start()
+--   end,
+-- })
+--
+-- vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+-- vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
