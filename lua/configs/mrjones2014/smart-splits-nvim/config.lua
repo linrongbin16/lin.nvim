d4c35df6b285084a650ddb16a222f1c84e2fe30c
linrongbin16 <linrongbin16@outlook.com>
2025-10-21 09:26:04 +0800

    c

--- lua/configs/mrjones2014/smart-splits-nvim/config.lua
+++ lua/configs/mrjones2014/smart-splits-nvim/config.lua
@@ -6,8 +6,10 @@ require("smart-splits").setup({
   },
   -- Ignored filetypes (only while resizing)
   ignored_filetypes = {},
+
   -- the default number of lines/columns to resize by at a time
   default_amount = 10,
+
   -- Desired behavior when your cursor is at an edge and you
   -- are moving towards that same edge:
   -- 'wrap' => Wrap to opposite side
@@ -35,20 +37,24 @@ require("smart-splits").setup({
   -- NOTE: `at_edge = 'wrap'` is not supported on Kitty terminal
   -- multiplexer, as there is no way to determine layout via the CLI
   at_edge = "wrap",
+
   -- Desired behavior when the current window is floating:
   -- 'previous' => Focus previous Vim window and perform action
   -- 'mux' => Always forward action to multiplexer
   float_win_behavior = "previous",
+
   -- when moving cursor between splits left or right,
   -- place the cursor on the same row of the *screen*
   -- regardless of line numbers. False by default.
   -- Can be overridden via function parameter, see Usage.
   move_cursor_same_row = false,
+
   -- whether the cursor should follow the buffer when swapping
   -- buffers by default; it can also be controlled by passing
   -- `{ move_cursor = true }` or `{ move_cursor = false }`
   -- when calling the Lua function.
   cursor_follows_swapped_bufs = false,
+
   -- ignore these autocmd events (via :h eventignore) while processing
   -- smart-splits.nvim computations, which involve visiting different
   -- buffers and windows. These events will be ignored during processing,
@@ -58,6 +64,7 @@ require("smart-splits").setup({
     "BufEnter",
     "WinEnter",
   },
+
   -- enable or disable a multiplexer integration;
   -- automatically determined, unless explicitly disabled or set,
   -- by checking the $TERM_PROGRAM environment variable,
