4de17f4c30b5f7e6ea033a781d56270c6e5bc3d6
linrongbin16 <linrongbin16@outlook.com>
2025-12-04 10:56:03 +0800

    c

--- inst/apt.sh
+++ inst/apt.sh
@@ -29,10 +29,7 @@ install_go() {
 }
 
 install_lazygit() {
-	LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
-	curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-	tar xf lazygit.tar.gz lazygit
-	sudo install lazygit -D -t /usr/local/bin/
+	go install github.com/jesseduffield/lazygit@latest
 }
 
 install_eza() {
