e9a7be9769ba8ad334efdeec260cc384dc3e8770
linrongbin16 <linrongbin16@outlook.com>
2025-12-09 09:32:33 +0800

    fix(vim-gitgutter): remove deprecated 'gitgutter_grep'

--- lua/configs/airblade/vim-gitgutter/init.lua
+++ lua/configs/airblade/vim-gitgutter/init.lua
@@ -4,10 +4,5 @@ vim.g.gitgutter_map_keys = 0
 -- don't clobber diagnostic sign
 vim.g.gitgutter_sign_allow_clobber = 0
 
--- use rg to improve performance
-if vim.fn.executable("rg") > 0 then
-  vim.g.gitgutter_grep = "rg"
-end
-
 -- use float window for preview
 vim.g.gitgutter_preview_win_floating = 1
