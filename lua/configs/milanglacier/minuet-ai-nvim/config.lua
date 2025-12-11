4fdbc1e8f16300a9f949190af645fad92c741386
linrongbin16 <linrongbin16@outlook.com>
2025-12-11 10:09:34 +0800

    c

--- lua/configs/milanglacier/minuet-ai-nvim/config.lua
+++ lua/configs/milanglacier/minuet-ai-nvim/config.lua
@@ -42,7 +42,7 @@ require("minuet").setup({
 --
 -- Run (and initialize) llama.cpp with Qwen2.5-Coder model:
 --
--- For better performance and more memory-usage:
+-- For more performance/memory-usage:
 -- ```
 -- llama-server \
 --    -hf ggml-org/Qwen2.5-Coder-3B-Q8_0-GGUF \
@@ -50,7 +50,7 @@ require("minuet").setup({
 --    --ctx-size 0 --cache-reuse 1024
 -- ```
 --
--- For worse performance but lower memory-usage:
+-- For less performance/memory-usage:
 -- ```
 -- llama-server \
 --    -hf ggml-org/Qwen2.5-Coder-1.5B-Q8_0-GGUF \
