3b6e4d83485e52de20f45790c4e3b2363fce9bd2
linrongbin16 <linrongbin16@outlook.com>
2025-12-11 09:20:36 +0800

    c

--- lua/plugins/users_sample.lua
+++ lua/plugins/users_sample.lua
@@ -92,15 +92,11 @@
 --   {
 --     "saecki/crates.nvim",
 --     version = "*",
---     config = function()
---       require("crates").setup()
---     end,
+--     opts = {},
 --   },
 --   {
 --     "vuki656/package-info.nvim",
---     config = function()
---       require("package-info").setup()
---     end,
+--     opts = {},
 --   },
 -- }
 
