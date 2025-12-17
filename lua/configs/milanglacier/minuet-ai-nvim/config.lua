cd8ab51282083a77c7dc28d42e4f5450269c7dcf
linrongbin16 <linrongbin16@outlook.com>
2025-12-17 14:27:03 +0800

    c

--- lua/configs/milanglacier/minuet-ai-nvim/config.lua
+++ lua/configs/milanglacier/minuet-ai-nvim/config.lua
@@ -1,33 +1,48 @@
 require("minuet").setup({
   provider = "openai_fim_compatible",
   n_completions = 1, -- recommend for local model for resource saving
-  context_window = 1024,
+  context_window = 4096,
   provider_options = {
+
+    -- llama.cpp
+    --
+    -- openai_fim_compatible = {
+    --   -- For Windows users, TERM may not be present in environment variables.
+    --   -- Consider using APPDATA instead.
+    --   api_key = "TERM",
+    --   name = "Llama.cpp",
+    --   end_point = "http://localhost:8012/v1/completions",
+    --   -- The model is set by the llama-cpp server and cannot be altered
+    --   -- post-launch.
+    --   model = "PLACEHOLDER",
+    --   optional = {
+    --     max_tokens = 56,
+    --     top_p = 0.9,
+    --   },
+    --   -- Llama.cpp does not support the `suffix` option in FIM completion.
+    --   -- Therefore, we must disable it and manually populate the special
+    --   -- tokens required for FIM completion.
+    --   template = {
+    --     prompt = function(context_before_cursor, context_after_cursor, _)
+    --       return "<|fim_prefix|>"
+    --         .. context_before_cursor
+    --         .. "<|fim_suffix|>"
+    --         .. context_after_cursor
+    --         .. "<|fim_middle|>"
+    --     end,
+    --     suffix = false,
+    --   },
+    -- },
+
+    -- Ollama
     openai_fim_compatible = {
-      -- For Windows users, TERM may not be present in environment variables.
-      -- Consider using APPDATA instead.
       api_key = "TERM",
-      name = "Llama.cpp",
-      end_point = "http://localhost:8012/v1/completions",
-      -- The model is set by the llama-cpp server and cannot be altered
-      -- post-launch.
-      model = "PLACEHOLDER",
+      name = "Ollama",
+      end_point = "http://localhost:11434/v1/completions",
+      model = "qwen2.5-coder:14b",
       optional = {
         max_tokens = 56,
         top_p = 0.9,
-      },
-      -- Llama.cpp does not support the `suffix` option in FIM completion.
-      -- Therefore, we must disable it and manually populate the special
-      -- tokens required for FIM completion.
-      template = {
-        prompt = function(context_before_cursor, context_after_cursor, _)
-          return "<|fim_prefix|>"
-            .. context_before_cursor
-            .. "<|fim_suffix|>"
-            .. context_after_cursor
-            .. "<|fim_middle|>"
-        end,
-        suffix = false,
       },
     },
   },
