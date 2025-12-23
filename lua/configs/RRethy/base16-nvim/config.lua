66b6545d2e5d101f9fa2b84584de4c2b28dd3828
linrongbin16 <linrongbin16@outlook.com>
2025-12-23 16:37:54 +0800

    c

--- lua/configs/RRethy/base16-nvim/config.lua
+++ lua/configs/RRethy/base16-nvim/config.lua
@@ -1,6 +1,20 @@
-local colors = vim.fn.getcompletion("", "color")
-local n = #colors
-local s = os.time()
-local r = math.fmod(s, n)
-local color = colors[r]
+local str = require("commons.str")
+
+local rtp = vim.fn.escape(vim.o.runtimepath, " ")
+local colorfiles = vim.fn.globpath(rtp, "colors/*.vim", 0, 1)
+local n = #colorfiles
+local ts = os.time()
+local r = math.fmod(ts, n)
+
+local colorfile = colorfiles[r]
+local color = vim.fn.fnamemodify(colorfile, ":t:r")
+print(string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color)))
+
+while str.endswith(colorfile, "-light") or str.endswith(colorfile, "-day") do
+  r = math.fmod(r + 1, n)
+  colorfile = colorfiles[r]
+  color = vim.fn.fnamemodify(colorfile, ":t:r")
+  print(string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color)))
+end
+
 vim.cmd("colorscheme " .. color)
