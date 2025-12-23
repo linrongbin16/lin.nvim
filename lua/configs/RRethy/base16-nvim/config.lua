ca42ce8d64ed7a43631b4c7a4eeaa32f496e871f
linrongbin16 <linrongbin16@outlook.com>
2025-12-23 16:40:14 +0800

    c

--- lua/configs/RRethy/base16-nvim/config.lua
+++ lua/configs/RRethy/base16-nvim/config.lua
@@ -8,19 +8,19 @@ local r = math.fmod(ts, n)
 
 local colorfile = colorfiles[r]
 local color = vim.fn.fnamemodify(colorfile, ":t:r")
-vim.defer_fn(function()
-  print(string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color)))
-end, 1000)
+-- vim.defer_fn(function()
+--   print(string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color)))
+-- end, 1000)
 
 while str.endswith(color, "-light") or str.endswith(color, "-day") do
   r = math.fmod(r + 1, n)
   colorfile = colorfiles[r]
   color = vim.fn.fnamemodify(colorfile, ":t:r")
-  vim.defer_fn(function()
-    print(
-      string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color))
-    )
-  end, 2000)
+  -- vim.defer_fn(function()
+  --   print(
+  --     string.format("r:%s,f:%s,c:%s", vim.inspect(r), vim.inspect(colorfile), vim.inspect(color))
+  --   )
+  -- end, 2000)
 end
 
 vim.cmd("colorscheme " .. color)
