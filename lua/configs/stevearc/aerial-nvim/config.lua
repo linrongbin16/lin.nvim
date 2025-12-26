bd54fe674e8e78af5500d78db40b38021af6efc8
linrongbin16 <linrongbin16@outlook.com>
2025-12-26 17:13:15 +0800

    c

--- lua/configs/stevearc/aerial-nvim/config.lua
+++ lua/configs/stevearc/aerial-nvim/config.lua
@@ -1,1 +1,9 @@
-require("aerial").setup({})
+local constants = require("builtin.constants")
+
+require("aerial").setup({
+  layout = {
+    width = constants.layout.sidebar.scale,
+  },
+  update_events = "TextChanged,InsertLeave,WinEnter,BufEnter,BufNewFile,BufReadPost,BufWinEnter,BufWinLeave,TabEnter,BufWritePost,CmdlineLeave,CmdwinLeave,TermLeave,TermClose,DirChanged,DiffUpdated,FocusGained,FocusLost",
+  show_guides = true,
+})
