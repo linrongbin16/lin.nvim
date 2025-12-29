4061957fd0707757bc253ee4f4940fcc61cf0a1f
linrongbin16 <linrongbin16@outlook.com>
2025-12-29 14:59:45 +0800

    refactor(fzf): add multiple actions/keymaps for fzf-lua (#807)

--- lua/configs/ibhagwan/fzf-lua/config.lua
+++ lua/configs/ibhagwan/fzf-lua/config.lua
@@ -1,5 +1,21 @@
 local constants = require("builtin.constants")
 
+local basic_actions = {
+  ["enter"] = require("fzf-lua").actions.file_edit,
+  ["ctrl-s"] = false,
+  ["ctrl-v"] = false,
+  ["ctrl-t"] = false,
+}
+
+local toggle_ignore_actions = vim.tbl_deep_extend("force", basic_actions, {
+  ["ctrl-i"] = require("fzf-lua").actions.toggle_ignore,
+})
+
+local fzf_keymaps = {
+  ["ctrl-g"] = "toggle",
+  ["ctrl-p"] = "toggle-preview",
+}
+
 require("fzf-lua").setup({
   { "fzf-native" },
   winopts = {
@@ -16,17 +32,29 @@ require("fzf-lua").setup({
       border = constants.window.border,
     },
   },
+  keymap = {
+    fzf = fzf_keymaps,
+  },
+  actions = {
+    files = toggle_ignore_actions,
+    git = {
+      files = basic_actions,
+    },
+    grep = toggle_ignore_actions,
+    buffers = basic_actions,
+  },
   files = {
     cwd_prompt = false,
-    actions = {
-      ["ctrl-r"] = { require("fzf-lua").actions.toggle_ignore },
+    hidden = true,
+  },
+  git = {
+    files = {
+      cwd_prompt = false,
     },
   },
   grep = {
     prompt = "Live Grep> ",
-    actions = {
-      ["ctrl-r"] = { require("fzf-lua").actions.toggle_ignore },
-    },
+    hidden = true,
     rg_glob_fn = function(query, opts)
       local regex, flags = query:match("^(.-)%s%-%-(.*)$")
       return (regex or query), flags
