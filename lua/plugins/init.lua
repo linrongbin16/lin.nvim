46771bb7471bfba9b62407d4bbd59c7481f0ca8e
linrongbin16 <linrongbin16@outlook.com>
2025-12-30 17:21:33 +0800

    c

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -242,11 +242,7 @@ local M = {
     "folke/lazydev.nvim",
     ft = "lua",
     dependencies = { "Bilal2453/luvit-meta" },
-    opts = {
-      library = {
-        { path = "luvit-meta/library", words = { "vim%.uv", "uv%." } },
-      },
-    },
+    config = lua_config("folke/lazydev.nvim"),
   },
   {
     "L3MON4D3/LuaSnip",
