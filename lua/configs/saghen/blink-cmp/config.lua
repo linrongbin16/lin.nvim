6e7abf7eb2e1d57a20b69291e0dcc4f88191e37d
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2025-12-31 12:54:01 +0000

    chore(pr): auto-commit

--- lua/configs/saghen/blink-cmp/config.lua
+++ lua/configs/saghen/blink-cmp/config.lua
@@ -52,31 +52,31 @@ require("blink.cmp").setup({
             highlight = "BlinkCmpSource",
           },
           kind_icon = {
-          text = function(ctx)
-            local icon = ctx.kind_icon
-            if vim.tbl_contains({ "Path" }, ctx.source_name) then
+            text = function(ctx)
+              local icon = ctx.kind_icon
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
                 local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
                 if dev_icon then
-                    icon = dev_icon
+                  icon = dev_icon
                 end
-            else
+              else
                 icon = require("lspkind").symbolic(ctx.kind, {
-                    mode = "symbol",
+                  mode = "symbol",
                 })
-            end
-            return icon .. ctx.icon_gap
-          end,
-          highlight = function(ctx)
-            local hl = ctx.kind_hl
-            if vim.tbl_contains({ "Path" }, ctx.source_name) then
-              local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-              if dev_icon then
-                hl = dev_hl
               end
-            end
-            return hl
-          end,
-          }
+              return icon .. ctx.icon_gap
+            end,
+            highlight = function(ctx)
+              local hl = ctx.kind_hl
+              if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                if dev_icon then
+                  hl = dev_hl
+                end
+              end
+              return hl
+            end,
+          },
         },
       },
     },
