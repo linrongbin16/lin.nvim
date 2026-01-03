5d1829ee8486e737cff05a724e8b9105f21d707a
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2026-01-03 11:56:33 +0000

    chore(pr): auto-commit

--- lua/configs/hrsh7th/nvim-cmp/config.lua
+++ lua/configs/hrsh7th/nvim-cmp/config.lua
@@ -33,7 +33,7 @@ local setup_opts = {
     format = function(entry, vim_item)
       if vim.tbl_contains({ "async_path" }, entry.source.name) then
         local icon, hl_group =
-            require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
+          require("nvim-web-devicons").get_icon(entry:get_completion_item().label)
         if icon then
           vim_item.kind = icon
           vim_item.kind_hl_group = hl_group
