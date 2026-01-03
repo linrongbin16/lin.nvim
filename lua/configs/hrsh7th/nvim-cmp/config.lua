bf3c2c4c79a8575bbe603948abfe14c7f8ca38b4
linrongbin16 <linrongbin16@outlook.com>
2026-01-03 14:46:46 +0800

    c

--- lua/configs/hrsh7th/nvim-cmp/config.lua
+++ lua/configs/hrsh7th/nvim-cmp/config.lua
@@ -26,7 +26,19 @@ local setup_opts = {
     { name = "nvim_lsp" },
     { name = "luasnip" },
   }, {
-    { name = "buffer" },
+    {
+      name = "buffer",
+      option = {
+        get_bufnrs = function()
+          local buf = vim.api.nvim_get_current_buf()
+          local byte_size = vim.api.nvim_buf_get_offset(buf, vim.api.nvim_buf_line_count(buf))
+          if byte_size > 1024 * 1024 then -- 1 Megabyte max
+            return {}
+          end
+          return { buf }
+        end,
+      },
+    },
     { name = "async_path" },
   }),
   formatting = {
