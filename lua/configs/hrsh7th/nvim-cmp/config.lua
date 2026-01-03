f499b25d5ac4538f621eb4641d95b411477be485
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2026-01-03 11:59:51 +0000

    chore(pr): auto-commit

--- lua/configs/hrsh7th/nvim-cmp/config.lua
+++ lua/configs/hrsh7th/nvim-cmp/config.lua
@@ -3,9 +3,9 @@ local compare = require("cmp.config.compare")
 local has_words_before = function()
   unpack = unpack or table.unpack
   local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+  return col ~= 0
+    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
-
 
 -- nvim_lsp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
