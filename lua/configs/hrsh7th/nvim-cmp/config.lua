12c3e64146f2fc98c83e3239e067e2378e827dc0
linrongbin16 <linrongbin16@outlook.com>
2026-01-03 19:59:10 +0800

    c

--- lua/configs/hrsh7th/nvim-cmp/config.lua
+++ lua/configs/hrsh7th/nvim-cmp/config.lua
@@ -1,5 +1,12 @@
 local compare = require("cmp.config.compare")
 
+local has_words_before = function()
+  unpack = unpack or table.unpack
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+
 -- nvim_lsp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 vim.lsp.config("*", {
@@ -86,17 +93,17 @@ local setup_opts = {
     end, { "i", "s" }),
     ["<Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
-        cmp.select_next_item()
+        cmp.confirm({ select = true })
       elseif luasnip.locally_jumpable(1) then
         luasnip.jump(1)
+      elseif has_words_before() then
+        cmp.complete()
       else
         fallback()
       end
     end, { "i", "s" }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.locally_jumpable(-1) then
+      if luasnip.locally_jumpable(-1) then
         luasnip.jump(-1)
       else
         fallback()
