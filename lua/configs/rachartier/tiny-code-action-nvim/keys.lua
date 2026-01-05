88a325e88566c85605a33fe0a8862edee50d12ef
linrongbin16 <linrongbin16@outlook.com>
2026-01-05 12:02:41 +0800

    c

--- lua/configs/rachartier/tiny-code-action-nvim/keys.lua
+++ lua/configs/rachartier/tiny-code-action-nvim/keys.lua
@@ -1,12 +1,9 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-  set_lazy_key("n", "<leader>ca", function()
+  set_lazy_key({ "n", "x" }, "<leader>ca", function()
     require("tiny-code-action").code_action()
-  end, { desc = "Run LSP code action" }),
-  set_lazy_key("x", "<leader>ca", function()
-    require("tiny-code-action").code_action()
-  end, { desc = "Run LSP code action on visual selection" }),
+  end, { desc = "Code Actions" }),
 }
 
 return M
