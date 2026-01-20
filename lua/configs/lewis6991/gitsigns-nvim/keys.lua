16aaf705009f2c08a1d42845bd67572e1b01ca8d
linrongbin16 <linrongbin16@outlook.com>
2026-01-20 10:21:56 +0800

    feat(gitsigns): migrate to "gitsigns.nvim" from "vim-gitgutter" (#825)

---
+++ lua/configs/lewis6991/gitsigns-nvim/keys.lua
@@ -0,0 +1,23 @@
+local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
+
+local M = {
+  set_lazy_key("n", "]c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "]c", bang = true })
+    else
+      require("gitsigns").nav_hunk("next")
+    end
+  end, { desc = "Go to next git hunk" }),
+  set_lazy_key("n", "[c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "[c", bang = true })
+    else
+      require("gitsigns").nav_hunk("prev")
+    end
+  end, { desc = "Go to previous git hunk" }),
+  set_lazy_key("n", "<leader>gb", function()
+    vim.cmd("Gitsigns blame")
+  end, { desc = "Toggle buffer line blame" }),
+}
+
+return M
