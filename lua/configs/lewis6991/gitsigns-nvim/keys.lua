aba3480ecd99ee9d747a9746268b6f5a9f9f0ada
linrongbin16 <linrongbin16@outlook.com>
2026-01-20 09:50:29 +0800

    c

--- lua/configs/lewis6991/gitsigns-nvim/keys.lua
+++ lua/configs/lewis6991/gitsigns-nvim/keys.lua
@@ -1,8 +1,20 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-  set_lazy_key("n", "]c", "<Plug>(GitGutterNextHunk)", { desc = "Go to next git hunk" }),
-  set_lazy_key("n", "[c", "<Plug>(GitGutterPrevHunk)", { desc = "Go to previous git hunk" }),
+  set_lazy_key("n", "]c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "]c", bang = true })
+    else
+      require("gitsigns").nav_hunk("next")
+    end
+  end, { desc = "Go to next git hunk" }),
+  set_lazy_key("n", "[c", function()
+    if vim.wo.diff then
+      vim.cmd.normal({ "[c", bang = true })
+    else
+      require("gitsigns").nav_hunk("prev")
+    end
+  end, { desc = "Go to previous git hunk" }),
 }
 
 return M
