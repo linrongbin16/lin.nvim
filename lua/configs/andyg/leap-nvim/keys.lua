1e873678742945ab2485bf3c97fdddb2612680f2
linrongbin16 <linrongbin16@outlook.com>
2026-03-12 22:30:27 +0800

    c

--- lua/configs/andyg/leap-nvim/keys.lua
+++ lua/configs/andyg/leap-nvim/keys.lua
@@ -1,21 +1,10 @@
 local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
 
 local M = {
-  set_lazy_key(
-    { "n", "x", "o" },
-    "s",
-    "<Plug>(leap-forward)",
-    { desc = "Jump forward with s{char1}{char2}" }
-  ),
-  set_lazy_key(
-    { "n", "x", "o" },
-    "S",
-    "<Plug>(leap-backward)",
-    { desc = "Jump backward with s{char1}{char2}" }
-  ),
+  set_lazy_key({ "n", "x", "o" }, "s", "<Plug>(leap)", { desc = "Jump with s{char1}{char2}" }),
   set_lazy_key(
     "n",
-    "gs",
+    "S",
     "<Plug>(leap-from-window)",
     { desc = "Jump to other windows with s{char1}{char2}" }
   ),
