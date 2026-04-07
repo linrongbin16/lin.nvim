8d8e357e83b5c83fdd1223efbf53f6a09ad58c4b
linrongbin16 <linrongbin16@outlook.com>
2026-04-07 10:43:46 +0800

    c

--- lua/builtin/key.lua
+++ lua/builtin/key.lua
@@ -1,21 +1,6 @@
--- Disable useless builtin plugins
+-- ---- KEY ----
+-- Disable unused builtin key mappings
 
--- Replace with 'neo-tree.nvim'
-vim.g.loaded_netrwPlugin = 1
-
--- Replace with 'vim-matchup'
-vim.g.loaded_matchit = 1
-vim.g.loaded_matchparen = 1
-
--- Not used
-vim.g.loaded_gzip = 1
-vim.g.loaded_tarPlugin = 1
-vim.g.loaded_zipPlugin = 1
-vim.g.loaded_tutor = 1
-vim.g.loaded_tohtml = 1
-vim.g.loaded_man = 1
-
--- Unused key mappings
 local bracket_pairs = {
   {
     mode = "x",
