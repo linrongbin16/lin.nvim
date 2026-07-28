1a2316167f5fcab9cbc2b03afb815e95ed9a22ec
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2026-07-28 05:28:59 +0000

    chore(pr): auto-commit

--- lua/configs/smoka7/hop-nvim/keys.lua
+++ lua/configs/smoka7/hop-nvim/keys.lua
@@ -1,8 +1,18 @@
 local keymap = require("util.keymap")
 
 local M = {
-  keymap.set_lazily({ "n", "x" }, "<leader>j", "<cmd>HopLineAC<cr>", { desc = "Jump to below lines" }),
-  keymap.set_lazily({ "n", "x" }, "<leader>k", "<cmd>HopLineBC<cr>", { desc = "Jump to above lines" }),
+  keymap.set_lazily(
+    { "n", "x" },
+    "<leader>j",
+    "<cmd>HopLineAC<cr>",
+    { desc = "Jump to below lines" }
+  ),
+  keymap.set_lazily(
+    { "n", "x" },
+    "<leader>k",
+    "<cmd>HopLineBC<cr>",
+    { desc = "Jump to above lines" }
+  ),
 }
 
 return M
