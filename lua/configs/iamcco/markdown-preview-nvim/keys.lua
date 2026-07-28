9850b27c07a607636274922640979045b1dd7c4a
linrongbin16 <linrongbin16@outlook.com>
2026-07-28 05:19:02 +0000

    c

--- lua/configs/iamcco/markdown-preview-nvim/keys.lua
+++ lua/configs/iamcco/markdown-preview-nvim/keys.lua
@@ -1,7 +1,7 @@
-local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
+local keymap = require("util.keymap")
 
 local M = {
-  set_lazy_key(
+  keymap.set_lazily(
     "n",
     "<leader>mp",
     ":MarkdownPreview<CR>",
