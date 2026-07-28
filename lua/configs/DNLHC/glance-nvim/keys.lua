0b006a7ff4bfcfe973e432dc38300406d6d2e788
linrongbin16 <linrongbin16@outlook.com>
2026-07-28 03:56:48 +0000

    c

--- lua/configs/DNLHC/glance-nvim/keys.lua
+++ lua/configs/DNLHC/glance-nvim/keys.lua
@@ -1,15 +1,15 @@
-local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
+local keymap = require("util.keymap")
 
 local M = {
-  set_lazy_key("n", "gd", "<CMD>Glance definitions<CR>", { desc = "Go to LSP definition" }),
-  set_lazy_key("n", "gr", "<CMD>Glance references<CR>", { desc = "Go to LSP reference" }),
-  set_lazy_key(
+  keymap.set_lazily("n", "gd", "<CMD>Glance definitions<CR>", { desc = "Go to LSP definition" }),
+  keymap.set_lazily("n", "gr", "<CMD>Glance references<CR>", { desc = "Go to LSP reference" }),
+  keymap.set_lazily(
     "n",
     "gt",
     "<CMD>Glance type_definitions<CR>",
     { desc = "Go to LSP type definition" }
   ),
-  set_lazy_key("n", "gi", "<CMD>Glance implementations<CR>", { desc = "Go to LSP implementation" }),
+  keymap.set_lazily("n", "gi", "<CMD>Glance implementations<CR>", { desc = "Go to LSP implementation" }),
 }
 
 return M
