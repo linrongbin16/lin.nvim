91e5f84fdd838f69bdc320109d57bc664476aa2f
linrongbin16 <linrongbin16@outlook.com>
2026-07-28 05:25:57 +0000

    c

--- lua/configs/mrjones2014/smart-splits-nvim/keys.lua
+++ lua/configs/mrjones2014/smart-splits-nvim/keys.lua
@@ -1,10 +1,10 @@
-local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
+local keymap = require("util.keymap")
 
 local M = {
-  set_lazy_key("n", "<leader>>", function()
+  keymap.set_lazily("n", "<leader>>", function()
     require("smart-splits").resize_right()
   end, { desc = "Resize window right" }),
-  set_lazy_key("n", "<leader><", function()
+  keymap.set_lazily("n", "<leader><", function()
     require("smart-splits").resize_left()
   end, { desc = "Resize window left" }),
 }
--- lua/configs/rachartier/tiny-code-action-nvim/keys.lua
+++ lua/configs/rachartier/tiny-code-action-nvim/keys.lua
@@ -1,7 +1,7 @@
-local set_lazy_key = require("builtin.utils.keymap").set_lazy_key
+local keymap = require("util.keymap")
 
 local M = {
-  set_lazy_key({ "n", "x" }, "<leader>ca", function()
+  keymap.set_lazily({ "n", "x" }, "<leader>ca", function()
     require("tiny-code-action").code_action()
   end, { desc = "Code Actions" }),
 }
