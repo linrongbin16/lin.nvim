85cab168a03edee84f4f9263ca6d0274ea05107f
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2026-07-28 05:21:45 +0000

    chore(pr): auto-commit

--- lua/configs/inkarkat/vim-mark/keys.lua
+++ lua/configs/inkarkat/vim-mark/keys.lua
@@ -3,13 +3,28 @@ local keymap = require("util.keymap")
 local M = {
   -- toggle cursor word in normal/visual mode
   keymap.set_lazily("n", "<leader>mk", "<Plug>MarkSet", { desc = "Toggle highlighting mark" }),
-  keymap.set_lazily("x", "<leader>mk", "<Plug>MarkIWhiteSet", { desc = "Toggle highlighting mark" }),
+  keymap.set_lazily(
+    "x",
+    "<leader>mk",
+    "<Plug>MarkIWhiteSet",
+    { desc = "Toggle highlighting mark" }
+  ),
   -- clear all words
-  keymap.set_lazily("n", "<leader>mK", "<Plug>MarkAllClear", { desc = "Clear all highlighting marks" }),
+  keymap.set_lazily(
+    "n",
+    "<leader>mK",
+    "<Plug>MarkAllClear",
+    { desc = "Clear all highlighting marks" }
+  ),
 
   -- search next/previous word
   keymap.set_lazily("n", "<leader>mn", "<Plug>MarkSearchNext", { desc = "Next highlighting mark" }),
-  keymap.set_lazily("n", "<leader>mN", "<Plug>MarkSearchPrev", { desc = "Previous highlighting mark" }),
+  keymap.set_lazily(
+    "n",
+    "<leader>mN",
+    "<Plug>MarkSearchPrev",
+    { desc = "Previous highlighting mark" }
+  ),
 }
 
 return M
