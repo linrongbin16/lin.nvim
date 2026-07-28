791b969f1ef1da1e6dbf0ca10ec4bb32bb770562
linrongbin16 <6496887+linrongbin16@users.noreply.github.com>
2026-07-28 04:03:18 +0000

    chore(pr): auto-commit

--- lua/configs/gbprod/yanky-nvim/keys.lua
+++ lua/configs/gbprod/yanky-nvim/keys.lua
@@ -2,8 +2,18 @@ local keymap = require("util.keymap")
 
 local M = {
   -- put
-  keymap.set_lazily({ "n", "x" }, "p", "<Plug>(YankyPutAfter)", { desc = "Put after cursor (yanky)" }),
-  keymap.set_lazily({ "n", "x" }, "P", "<Plug>(YankyPutBefore)", { desc = "Put before cursor (yanky)" }),
+  keymap.set_lazily(
+    { "n", "x" },
+    "p",
+    "<Plug>(YankyPutAfter)",
+    { desc = "Put after cursor (yanky)" }
+  ),
+  keymap.set_lazily(
+    { "n", "x" },
+    "P",
+    "<Plug>(YankyPutBefore)",
+    { desc = "Put before cursor (yanky)" }
+  ),
   keymap.set_lazily(
     { "n", "x" },
     "gp",
