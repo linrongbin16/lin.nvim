93f92a7bfdb7049f6c6ef649f38ac0964f028977
linrongbin16 <linrongbin16@outlook.com>
2026-08-07 17:25:15 +0800

    c

--- lua/configs/stevearc/oil-nvim/config.lua
+++ lua/configs/stevearc/oil-nvim/config.lua
@@ -1,1 +1,8 @@
-require("oil").setup()
+require("oil").setup({
+  delete_to_trash = true,
+  skip_confirm_for_simple_edits = true,
+  lsp_file_methods = {
+    enabled = true,
+    timeout_ms = 3000,
+  },
+})
