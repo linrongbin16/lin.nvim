5fa5863e21d53a505d4cd1ba075222ab66ec922b
linrongbin16 <linrongbin16@outlook.com>
2026-09-03 15:54:32 +0800

    feat(toggleterm): use 'tab' for toggleterm

--- lua/configs/akinsho/toggleterm-nvim/config.lua
+++ lua/configs/akinsho/toggleterm-nvim/config.lua
@@ -1,13 +1,5 @@
-local constants = require("api.constants")
-local layout = require("api.layout")
-
 require("toggleterm").setup({
   persist_size = false,
-  direction = "float",
-  float_opts = {
-    border = constants.window.border,
-    winblend = constants.window.blend,
-    width = layout.editor.width(constants.layout.window.scale, 10),
-    height = layout.editor.height(constants.layout.window.scale, 10),
-  },
+  persist_mode = false,
+  direction = "tab",
 })
