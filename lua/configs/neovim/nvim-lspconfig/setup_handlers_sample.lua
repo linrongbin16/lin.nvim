cff363b239ecfdb6b69a6e9c46d40b9132a2f7c2
linrongbin16 <linrongbin16@outlook.com>
2026-09-21 14:13:28 +0800

    fix(lspconfig): fix nvim-lspconfig initialize

--- lua/configs/neovim/nvim-lspconfig/setup_handlers_sample.lua
+++ lua/configs/neovim/nvim-lspconfig/setup_handlers_sample.lua
@@ -1,38 +1,13 @@
 -- Please copy this file to 'setup_handlers.lua' to enable it.
 
 -- Configure lsp server setup handlers.
--- This module will be passed to `vim.lsp.config`, and lsp servers will be automatically started by mason-lspconfig.
+-- This module will be passed to `vim.lsp.config`, only use this if mason cannot work.
 
 local setup_handlers = {
-  -- Please uncomment below lines to enable them.
-
-  -- clangd = {
-  --   capabilities = {
-  --     offsetEncoding = { "utf-16" },
-  --     general = {
-  --       positionEncodings = { "utf-16" },
-  --     },
-  --   },
-  -- },
-  -- flow = {},
-  -- lua_ls = {
-  --   settings = {
-  --     Lua = {
-  --       workspace = {
-  --         checkThirdParty = false,
-  --       },
-  --     },
-  --   },
-  -- },
-  -- ["rust_analyzer"] = {
-  --   capabilities = {
-  --     offsetEncoding = { "utf-16" },
-  --     general = {
-  --       positionEncodings = { "utf-16" },
-  --     },
-  --   },
-  -- },
-  -- tsserver = {},
+  clangd = {
+    cmd = { "clangd", "--background-index" },
+    filetypes = { "c", "cpp", "objc", "objcpp", "cuda" },
+  },
 }
 
 return setup_handlers
